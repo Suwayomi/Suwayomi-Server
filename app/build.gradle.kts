94f6a53be745d5aefc7e6c01613d10fef11353de
Aria Moradi <aria.moradi007@gmail.com>
2020-12-24 02:29:19 +0330

    removed submodule

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
 
     // api
     implementation("io.javalin:javalin:3.12.0")
+    implementation("org.slf4j:slf4j-simple:1.8.0-beta4")
+    implementation("org.slf4j:slf4j-api:1.8.0-beta4")
 
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
--- app/src/main/kotlin/ir/armor/tachidesk/Main.kt
+++ app/src/main/kotlin/ir/armor/tachidesk/Main.kt
@@ -75,7 +75,7 @@ class Main {
 
         @JvmStatic
         fun main(args: Array<String>) {
-            val app = Javalin.create().start(7000)
+            val app = Javalin.create().start(4567)
             app["/", { ctx: Context -> ctx.result("Hello World") }]
         }
     }
