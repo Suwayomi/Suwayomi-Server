a3b17365b7a82d9690c0ef0a918000e58c0e7f74
Aria Moradi <aria.moradi007@gmail.com>
2021-01-22 01:55:41 +0330

    [RELEASE CI] fix build issues

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cp ../master/repo/server-r* .
+cp ../master/repo/* .
 new_build=$(ls | tail -1)
 echo "New build file name: $new_build"
+
+cp -f $new_build Tachidesk-latest.jar
 
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
--- .github/scripts/create-repo.sh
+++ .github/scripts/create-repo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+
 
 mkdir -p repo/
 
