466f21a7e8cae800fd27b53ffa78a718f957f4b5
Aria Moradi <aria.moradi007@gmail.com>
2021-01-21 22:27:03 +0330

    set -e is the poison

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-set -e
 
 cp ../master/repo/server-r* .
 new_build=$(ls | tail -1)
+echo "$new_build"
 
-diff $new_build server-latest.jar
-if [ "$?" -eq 1 ]; then
+diff $new_build server-latest.jar > /dev/null
+if [ $? -eq 1 ]; then
   echo "copy"
   cp -f $new_build server-latest.jar
 else
   echo "rm"
   rm $new_build
 fi
+echo "shit"
 
 
-git config --global user.email "github-actions[bot]@users.noreply.github.com"
-git config --global user.name "github-actions[bot]"
+#git config --global user.email "github-actions[bot]@users.noreply.github.com"
+#git config --global user.name "github-actions[bot]"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
