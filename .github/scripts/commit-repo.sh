26a7e2f1cd3d82554670bd241c911170cc6fadfc
Aria Moradi <aria.moradi007@gmail.com>
2021-01-21 16:27:27 +0330

    fix again?

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -6,8 +6,10 @@ new_build=$(ls | tail -1)
 
 diff $new_build server-latest.jar
 if [ "$?" -eq 1 ]; then
+  echo "copy"
   cp -f $new_build server-latest.jar
 else
+  echo "rm"
   rm $new_build
 fi
 
@@ -17,7 +19,7 @@ git config --global user.name "github-actions[bot]"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
-    git commit -m "Update extensions repo"
+    git commit -m "Update repo"
     git push
 else
     echo "No changes to commit"
