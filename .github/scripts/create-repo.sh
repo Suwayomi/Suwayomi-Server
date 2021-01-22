b854fdeadbdd77539f60f746dfc4b448daba3d5d
Aria Moradi <aria.moradi007@gmail.com>
2021-01-22 11:50:33 +0330

    fix matching

--- .github/scripts/create-repo.sh
+++ .github/scripts/create-repo.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
-mkdir -p repo/
-
 # Get last commit message
 last_commit_log=$(git log -1 --pretty=format:"%s")
 echo "last commit log: $last_commit_log"
 
-filter_count=$(echo "$last_commit_log" | grep -c "[RELEASE CI]" )
+filter_count=$(echo "$last_commit_log" | grep -c '\[RELEASE CI\]' )
+echo "count is: $filter_count"
 
 if [ "$filter_count" -gt 0 ]; then
+  mkdir -p repo/
   cp server/build/Tachidesk-*.jar repo/
 fi
