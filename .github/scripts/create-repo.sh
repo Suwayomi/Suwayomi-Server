0f6749b0c15765ca88d6416bd8c1e9d1cf93dcd0
Aria Moradi <aria.moradi007@gmail.com>
2021-02-04 04:48:15 +0330

    now support backward writing!

--- .github/scripts/create-repo.sh
+++ .github/scripts/create-repo.sh
@@ -4,7 +4,7 @@
 last_commit_log=$(git log -1 --pretty=format:"%s")
 echo "last commit log: $last_commit_log"
 
-filter_count=$(echo "$last_commit_log" | grep -c '\[RELEASE CI\]' )
+filter_count=$(echo "$last_commit_log" | grep -e '\[RELEASE CI\]' -e '\[CI RELEASE\]' | wc -c)
 echo "count is: $filter_count"
 
 if [ "$filter_count" -gt 0 ]; then
