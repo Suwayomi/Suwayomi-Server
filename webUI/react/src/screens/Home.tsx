61aee2e784bb195eb452e32755d065d830fe0d4e
Aria Moradi <aria.moradi007@gmail.com>
2021-02-04 18:02:34 +0330

    hint added

--- webUI/react/src/screens/Home.tsx
+++ webUI/react/src/screens/Home.tsx
@@ -7,7 +7,7 @@ import React from 'react';
 export default function Home() {
     return (
         <h1>
-            Home
+            Hint: Click Tn The Top Left Menu Button
         </h1>
     );
 }
