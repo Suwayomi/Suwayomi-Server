6506c84b857953b2a4699a6e56d6ee747ef32dab
Aria Moradi <aria.moradi007@gmail.com>
2021-02-08 05:36:19 +0330

    publish?

--- .github/scripts/create-repo.sh
+++ .github/scripts/create-repo.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Get last commit message
-last_commit_log=$(git log -1 --pretty=format:"%s")
-echo "last commit log: $last_commit_log"
+#last_commit_log=$(git log -1 --pretty=format:"%s")
+#echo "last commit log: $last_commit_log"
+#
+#filter_count=$(echo "$last_commit_log" | grep -e '\[RELEASE CI\]' -e '\[CI RELEASE\]' | wc -c)
+#echo "count is: $filter_count"
 
-filter_count=$(echo "$last_commit_log" | grep -e '\[RELEASE CI\]' -e '\[CI RELEASE\]' | wc -c)
-echo "count is: $filter_count"
+mkdir -p repo/
+cp server/build/Tachidesk-*.jar repo/
+cp server/build/Tachidesk-*.zip repo/
 
-if [ "$filter_count" -gt 0 ]; then
-  mkdir -p repo/
-  cp server/build/Tachidesk-*.jar repo/
-  cp server/build/Tachidesk-*.zip repo/
-fi
+
+#if [ "$filter_count" -gt 0 ]; then
+#  cp server/build/Tachidesk-*.jar repo/
+#  cp server/build/Tachidesk-*.zip repo/
+#fi
