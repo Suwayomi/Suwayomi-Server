63e5e1b45fc669c5893f1fbf448ab9d339829dfd
Aria Moradi <aria.moradi007@gmail.com>
2021-04-18 19:50:26 +0430

    add indexer

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -8,6 +8,9 @@ echo "last jar build file name: $new_jar_build"
 
 cp -f $new_jar_build Tachidesk-latest.jar
 
+latest=$(ls *.jar | tail -n1 | cut -d"-" -f3 | cut -d"." -f1)
+echo "{ \"latest\": $latest }" > index.json
+
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 git status
