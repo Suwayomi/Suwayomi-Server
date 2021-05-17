5e0c7d3c9ddadc3d40062964b4d5034a4991cf6c
Aria Moradi <aria.moradi007@gmail.com>
2021-05-17 23:28:23 +0430

    ditch packr because it can't load extension jars

--- scripts/windows-bundler.sh
+++ scripts/windows-bundler.sh
@@ -6,11 +6,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-echo "Downloading packr jar..."
-
-packr="packr-all-4.0.0.jar"
-curl -L "https://github.com/libgdx/packr/releases/download/4.0.0/packr-all-4.0.0.jar" -o $packr
-
 echo "Downloading jre..."
 
 jre="OpenJDK8U-jre_x64_windows_hotspot_8u292b10.zip"
@@ -22,17 +17,15 @@ jar=$(ls ../server/build/Tachidesk-*.jar)
 jar_name=$(echo $jar | cut -d'/' -f4)
 release_name=$(echo $jar_name | cut -d'.' -f4 --complement)-win64
 
-cp $jar "Tachidesk.jar"
+# make release dir
+mkdir $release_name
 
-java -jar $packr \
-     --platform windows64 \
-     --jdk $jre \
-     --executable Tachidesk \
-     --classpath Tachidesk.jar \
-     --mainclass ir.armor.tachidesk.MainKt \
-     --vmargs Xmx4G \
-     --output $release_name
+# move jre
+mv jdk8u292-b10-jre $release_name/jre
 
+cp $jar $release_name/Tachidesk.jar
+
+cp resources/Tachidesk.bat $release_name
 cp resources/Tachidesk-debug.bat $release_name
 
 zip_name=$release_name.zip
