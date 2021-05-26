732777f412e45a75a2fd1eb7222e7ed969b6ddb8
arbuilder <arbuilder@users.noreply.github.com>
2021-05-26 15:58:44 +0530

    add docker build workflow

---
+++ docker/startup_script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ ! -f /home/arbuilder/.local/share/Tachidesk/docker_touchfile ]; then
+	touch /home/arbuilder/.local/share/Tachidesk/docker_touchfile
+	curl -s --create-dirs -L https://raw.githubusercontent.com/suwayomi/tachidesk/main/docker/server.conf -o /home/arbuilder/.local/share/Tachidesk/server.conf;
+fi
+echo ""
+echo ""
+echo "                                                                ************README***********"
+echo "                                         Read Readme from https://github.com/suwayomi/docker-tachidesk before running container"
+echo "                                                                *****************************"
+echo ""
+echo ""
+echo "The server is running on http://localhost:4567"
+echo "log file location inside the container  /home/arbuilder/.local/share/Tachidesk/logfile.log"
+exec java -jar "/home/arbuilder/startup/tachidesk_latest.jar" > /home/arbuilder/.local/share/Tachidesk/logfile.log 2>&1;
