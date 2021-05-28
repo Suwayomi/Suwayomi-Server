77f2f8cc18b1b493839f9d3ae860aa75975582c9
Aria Moradi <aria.moradi007@gmail.com>
2021-05-28 16:23:26 +0430

    add copyright notice to files that miss it

--- server/src/main/kotlin/suwayomi/tachidesk/impl/extension/github/OnlineExtension.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/impl/extension/github/OnlineExtension.kt
@@ -1,5 +1,12 @@
 package suwayomi.tachidesk.impl.extension.github
 
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 data class OnlineExtension(
     val name: String,
     val pkgName: String,
