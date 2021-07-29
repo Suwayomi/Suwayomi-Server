5b64bdc5b7fd31bb78e8e5ca240531c336e67481
Aria Moradi <aria.moradi007@gmail.com>
2021-07-29 23:26:23 +0430

    add copyright notices to Syer10's previous PR

--- AndroidCompat/src/main/java/xyz/nulldev/androidcompat/replace/java/text/NumberFormat.java
+++ AndroidCompat/src/main/java/xyz/nulldev/androidcompat/replace/java/text/NumberFormat.java
@@ -1,5 +1,12 @@
 package xyz.nulldev.androidcompat.replace.java.text;
 
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import com.ibm.icu.text.DisplayContext;
 import com.ibm.icu.util.Currency;
 import com.ibm.icu.util.CurrencyAmount;
--- AndroidCompat/src/main/java/xyz/nulldev/androidcompat/replace/java/util/Calendar.java
+++ AndroidCompat/src/main/java/xyz/nulldev/androidcompat/replace/java/util/Calendar.java
@@ -1,5 +1,12 @@
 package xyz.nulldev.androidcompat.replace.java.util;
 
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import com.ibm.icu.text.DateFormat;
 import com.ibm.icu.util.ULocale;
 
