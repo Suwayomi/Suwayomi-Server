b7a6d6cae811fd9c061f7652632cd026e686acdd
Aria Moradi <aria.moradi007@gmail.com>
2021-07-31 07:41:19 +0430

    fix HentaiHand preferences

---
+++ AndroidCompat/src/main/java/android/widget/EditText.java
@@ -0,0 +1,40 @@
+package android.widget;
+
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+public class EditText {
+    public EditText(android.content.Context context) { throw new RuntimeException("Stub!"); }
+
+    public EditText(android.content.Context context, android.util.AttributeSet attrs) { throw new RuntimeException("Stub!"); }
+
+    public EditText(android.content.Context context, android.util.AttributeSet attrs, int defStyleAttr) { throw new RuntimeException("Stub!"); }
+
+    public EditText(android.content.Context context, android.util.AttributeSet attrs, int defStyleAttr, int defStyleRes) { throw new RuntimeException("Stub!"); }
+
+    public boolean getFreezesText() { throw new RuntimeException("Stub!"); }
+
+    protected boolean getDefaultEditable() { throw new RuntimeException("Stub!"); }
+
+    protected android.text.method.MovementMethod getDefaultMovementMethod() { throw new RuntimeException("Stub!"); }
+
+    public android.text.Editable getText() { throw new RuntimeException("Stub!"); }
+
+    public void setText(java.lang.CharSequence text, android.widget.TextView.BufferType type) { throw new RuntimeException("Stub!"); }
+
+    public void setSelection(int start, int stop) { throw new RuntimeException("Stub!"); }
+
+    public void setSelection(int index) { throw new RuntimeException("Stub!"); }
+
+    public void selectAll() { throw new RuntimeException("Stub!"); }
+
+    public void extendSelection(int index) { throw new RuntimeException("Stub!"); }
+
+    public void setEllipsize(android.text.TextUtils.TruncateAt ellipsis) { throw new RuntimeException("Stub!"); }
+
+    public java.lang.CharSequence getAccessibilityClassName() { throw new RuntimeException("Stub!"); }
+}
--- AndroidCompat/src/main/java/androidx/preference/EditTextPreference.java
+++ AndroidCompat/src/main/java/androidx/preference/EditTextPreference.java
@@ -7,7 +7,11 @@ package androidx.preference;
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import android.annotation.NonNull;
+import android.annotation.Nullable;
 import android.content.Context;
+import android.widget.EditText;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class EditTextPreference extends Preference {
     // reference: https://android.googlesource.com/platform/frameworks/support/+/996971f962fcd554339a7cb2859cef9ca89dbcb7/preference/preference/src/main/java/androidx/preference/EditTextPreference.java
@@ -15,6 +19,9 @@ public class EditTextPreference extends Preference {
     private String text;
     private CharSequence dialogTitle;
     private CharSequence dialogMessage;
+
+    @JsonIgnore
+    private OnBindEditTextListener onBindEditTextListener;
 
     public EditTextPreference(Context context) {
         super(context);
@@ -42,5 +49,17 @@ public class EditTextPreference extends Preference {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public OnBindEditTextListener getOnBindEditTextListener() {
+        return onBindEditTextListener;
+    }
+
+    public void setOnBindEditTextListener(@Nullable OnBindEditTextListener onBindEditTextListener) {
+        this.onBindEditTextListener = onBindEditTextListener;
+    }
+
+    public interface OnBindEditTextListener {
+        void onBindEditText(@NonNull EditText editText);
     }
 }
