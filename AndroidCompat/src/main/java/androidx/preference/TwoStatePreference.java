c7112ec67f05df33535a73211a8ef3a29914f317
Aria Moradi <aria.moradi007@gmail.com>
2021-08-05 03:28:21 +0430

    mark methods as @JsonIgnore to avoid Jackson serializing them

--- AndroidCompat/src/main/java/androidx/preference/TwoStatePreference.java
+++ AndroidCompat/src/main/java/androidx/preference/TwoStatePreference.java
@@ -8,25 +8,35 @@ package androidx.preference;
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import android.content.Context;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class TwoStatePreference extends Preference {
+    // Note: remove @JsonIgnore and implement methods if any extension ever uses these methods or the variables behind them
 
     public TwoStatePreference(Context context) { super(context); }
 
+    @JsonIgnore
     public boolean isChecked() { throw new RuntimeException("Stub!"); }
 
+    @JsonIgnore
     public void setChecked(boolean checked) { throw new RuntimeException("Stub!"); }
 
+    @JsonIgnore
     public CharSequence getSummaryOn() { throw new RuntimeException("Stub!"); }
 
+    @JsonIgnore
     public void setSummaryOn(CharSequence summary) { throw new RuntimeException("Stub!"); }
 
+    @JsonIgnore
     public CharSequence getSummaryOff() { throw new RuntimeException("Stub!"); }
 
+    @JsonIgnore
     public void setSummaryOff(CharSequence summary) { throw new RuntimeException("Stub!"); }
 
+    @JsonIgnore
     public boolean getDisableDependentsState() { throw new RuntimeException("Stub!"); }
 
+    @JsonIgnore
     public void setDisableDependentsState(boolean disableDependentsState) { throw new RuntimeException("Stub!"); }
 
     /** Tachidesk specific API */
