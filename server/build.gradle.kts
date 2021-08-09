95b3587f7a33eb257b0dd2335428eb2c4c355f1a
Aria Moradi <aria.moradi007@gmail.com>
2021-08-10 02:04:35 +0430

    update webUI

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -102,7 +102,7 @@ sourceSets {
 
 // should be bumped with each stable release
 val tachideskVersion = System.getenv("ProductVersion") ?: "v0.4.4"
-val webUIRevisionTag = System.getenv("WebUIRevision") ?: "r20"
+val webUIRevisionTag = System.getenv("WebUIRevision") ?: "r21"
 
 // counts commit count on master
 val tachideskRevision = runCatching {
