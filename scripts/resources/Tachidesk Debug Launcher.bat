0c28320ce35b636115343a3d22ab131b8fca98b0
Aria Moradi <aria.moradi007@gmail.com>
2021-08-19 02:54:36 +0430

    better debug launcher

--- scripts/resources/Tachidesk Debug Launcher.bat
+++ scripts/resources/Tachidesk Debug Launcher.bat
@@ -1,1 +1,7 @@
+:: cleaner output
+@echo off
+
 jre\bin\java -Dsuwayomi.tachidesk.config.server.debugLogsEnabled=true -jar  Tachidesk.jar
+
+:: Prevent cmd from closing when Tachidesk crashes
+pause
