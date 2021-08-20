086a7603789eb753f498a5878611d8e289793c45
Aria Moradi <aria.moradi007@gmail.com>
2021-08-21 03:55:22 +0430

    update WebUI

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -14,7 +14,7 @@ const val MainClass = "suwayomi.tachidesk.MainKt"
 // should be bumped with each stable release
 val tachideskVersion = System.getenv("ProductVersion") ?: "v0.4.6"
 
-val webUIRevisionTag = System.getenv("WebUIRevision") ?: "r30"
+val webUIRevisionTag = System.getenv("WebUIRevision") ?: "r34"
 
 // counts commits on the the master branch
 val tachideskRevision = runCatching {
