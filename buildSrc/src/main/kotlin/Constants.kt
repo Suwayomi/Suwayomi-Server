ca3246de0255168f93fdc40bfdeedd3149821f52
Aria Moradi <aria.moradi007@gmail.com>
2021-09-02 15:21:27 +0430

    bump version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -14,7 +14,7 @@ const val MainClass = "suwayomi.tachidesk.MainKt"
 // should be bumped with each stable release
 val tachideskVersion = System.getenv("ProductVersion") ?: "v0.4.8"
 
-val webUIRevisionTag = System.getenv("WebUIRevision") ?: "r41"
+val webUIRevisionTag = System.getenv("WebUIRevision") ?: "r767"
 
 // counts commits on the the master branch
 val tachideskRevision = runCatching {
