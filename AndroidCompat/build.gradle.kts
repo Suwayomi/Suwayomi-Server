350574bb75724a47c30185d82036889ffdcd6316
Syer10 <syer10@users.noreply.github.com>
2021-09-18 13:03:24 -0400

    Cleanup and update gradle, update dependencies

--- AndroidCompat/build.gradle.kts
+++ AndroidCompat/build.gradle.kts
@@ -1,68 +1,28 @@
-
-plugins {
-    application
-    kotlin("plugin.serialization")
-}
-
-
-repositories {
-    mavenCentral()
-    maven {
-        url = uri("https://jitpack.io")
-    }
-
-    maven {
-        url = uri("https://maven.google.com")
-    }
-}
-
 dependencies {
     // Android stub library
     implementation(fileTree("lib/"))
 
-    // JSON
-    compileOnly("com.google.code.gson:gson:2.8.6")
-
     // XML
-    compileOnly(group= "xmlpull", name= "xmlpull", version= "1.1.3.1")
+    compileOnly("xmlpull:xmlpull:1.1.3.4a")
 
     // Config API
     implementation(project(":AndroidCompat:Config"))
 
     // APK sig verifier
-    compileOnly("com.android.tools.build:apksig:4.2.0-alpha13")
+    compileOnly("com.android.tools.build:apksig:7.1.0-alpha12")
 
     // AndroidX annotations
-    compileOnly("androidx.annotation:annotation:1.2.0-alpha01")
+    compileOnly("androidx.annotation:annotation:1.2.0")
 
     // substitute for duktape-android
     implementation("org.mozilla:rhino-runtime:1.7.13") // slimmer version of 'org.mozilla:rhino'
     implementation("org.mozilla:rhino-engine:1.7.13") // provides the same interface as 'javax.script' a.k.a Nashorn
 
     // Kotlin wrapper around Java Preferences, makes certain things easier
-    val multiplatformSettingsVersion = "0.7.7"
+    val multiplatformSettingsVersion = "0.8"
     implementation("com.russhwolf:multiplatform-settings-jvm:$multiplatformSettingsVersion")
     implementation("com.russhwolf:multiplatform-settings-serialization-jvm:$multiplatformSettingsVersion")
 
     // Android version of SimpleDateFormat
     implementation("com.ibm.icu:icu4j:69.1")
 }
-
-tasks {
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
-    }
-}
-
-//def fatJarTask = tasks.getByPath(':AndroidCompat:JVMPatch:fatJar')
-//
-//// Copy JVM core patches
-//task copyJVMPatches(type: Copy) {
-//    from fatJarTask.outputs.files
-//    into 'src/main/resources/patches'
-//}
-//
-//compileOnly(Java.dependsOn gradle.includedBuild('dex2jar').task(':dex-translator:assemble')
-//compileOnly(Java.dependsOn copyJVMPatches
-//copyJVMPatches.dependsOn fatJarTask
-//
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jmailen.gradle.kotlinter.tasks.FormatTask
+import org.jmailen.gradle.kotlinter.tasks.LintTask
 
 plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
+    id("org.jmailen.kotlinter") version "3.6.0"
 }
 
 allprojects {
@@ -12,10 +15,8 @@ allprojects {
 
     repositories {
         mavenCentral()
-        maven("https://maven.google.com/")
+        google()
         maven("https://jitpack.io")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
-        maven("https://dl.google.com/dl/android/maven2/")
     }
 }
 
@@ -27,17 +28,35 @@ val projects = listOf(
 
 configure(projects) {
     apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
 
     java {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_1_8.toString()
+    tasks {
+        withType<KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_1_8.toString()
+                freeCompilerArgs = listOf(
+                    "-Xopt-in=kotlin.RequiresOptIn",
+                    "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                    "-Xopt-in=kotlinx.coroutines.InternalCoroutinesApi",
+                    "-Xopt-in=kotlinx.serialization.ExperimentalSerializationApi",
+                )
+            }
+        }
+
+        withType<LintTask> {
+            source(files("src/kotlin"))
+        }
+
+        withType<FormatTask> {
+            source(files("src/kotlin"))
         }
     }
+
 
     dependencies {
         // Kotlin
@@ -46,7 +65,7 @@ configure(projects) {
         testImplementation(kotlin("test-junit5"))
 
         // coroutines
-        val coroutinesVersion = "1.5.1"
+        val coroutinesVersion = "1.5.2"
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutinesVersion")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
@@ -55,14 +74,13 @@ configure(projects) {
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinSerializationVersion")
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:$kotlinSerializationVersion")
 
-
         // Dependency Injection
-        implementation("org.kodein.di:kodein-di-conf-jvm:7.7.0")
+        implementation("org.kodein.di:kodein-di-conf-jvm:7.8.0")
 
         // Logging
-        implementation("org.slf4j:slf4j-api:1.7.30")
-        implementation("ch.qos.logback:logback-classic:1.2.3")
-        implementation("io.github.microutils:kotlin-logging:2.0.6")
+        implementation("org.slf4j:slf4j-api:1.7.32")
+        implementation("ch.qos.logback:logback-classic:1.2.6")
+        implementation("io.github.microutils:kotlin-logging:2.0.11")
 
         // ReactiveX
         implementation("io.reactivex:rxjava:1.3.8")
@@ -70,7 +88,7 @@ configure(projects) {
         implementation("com.jakewharton.rxrelay:rxrelay:1.2.0")
 
         // dependency both in AndroidCompat and extensions, version locked by Tachiyomi app/extensions
-        implementation("org.jsoup:jsoup:1.14.1")
+        implementation("org.jsoup:jsoup:1.14.2")
 
         // dependency of :AndroidCompat:Config
         implementation("com.typesafe:config:1.4.1")
@@ -86,7 +104,6 @@ configure(projects) {
 
         // APK parser
         implementation("net.dongliu:apk-parser:2.6.10")
-
 
         // dependency both in AndroidCompat and server, version locked by javalin
         implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.4")
