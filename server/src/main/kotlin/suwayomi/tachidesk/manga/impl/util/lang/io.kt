05ab011a285b6e1f16144d7d920562e7d6103f0f
Aria Moradi <aria.moradi007@gmail.com>
2021-11-17 23:19:00 +0330

    migrate application directories

---
+++ server/src/main/kotlin/suwayomi/tachidesk/manga/impl/util/lang/io.kt
@@ -0,0 +1,12 @@
+package suwayomi.tachidesk.manga.impl.util.lang
+
+import java.io.File
+
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */ 
+
+fun File.renameTo(newPath: String) = renameTo(File(newPath))
