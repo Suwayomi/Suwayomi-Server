7e532a2e9cdfbd9a11c756430bd50299ff72d345
Syer10 <syer10@users.noreply.github.com>
2021-12-19 12:26:47 -0500

    Add missing import

--- server/src/main/kotlin/suwayomi/tachidesk/manga/impl/util/BytecodeEditor.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/manga/impl/util/BytecodeEditor.kt
@@ -18,6 +18,7 @@ import org.objectweb.asm.Opcodes
 import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 import kotlin.streams.asSequence
 
 object BytecodeEditor {
