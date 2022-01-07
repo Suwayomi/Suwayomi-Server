bcd5970a6f5317c601e5ad2cc700181919e69c0e
Syer10 <syer10@users.noreply.github.com>
2022-01-06 23:02:39 -0500

    Update Gradle and Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jmailen.gradle.kotlinter.tasks.LintTask
 plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    id("org.jmailen.kotlinter") version "3.6.0"
+    id("org.jmailen.kotlinter") version "3.8.0"
     id("com.github.gmazzo.buildconfig") version "3.0.3" apply false
 }
 
@@ -69,22 +69,22 @@ configure(projects) {
         testImplementation(kotlin("test-junit5"))
 
         // coroutines
-        val coroutinesVersion = "1.5.2"
+        val coroutinesVersion = "1.6.0"
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutinesVersion")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
 
-        val kotlinSerializationVersion = "1.3.0-RC"
+        val kotlinSerializationVersion = "1.3.2"
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinSerializationVersion")
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:$kotlinSerializationVersion")
 
         // Dependency Injection
-        implementation("org.kodein.di:kodein-di-conf-jvm:7.8.0")
+        implementation("org.kodein.di:kodein-di-conf-jvm:7.10.0")
 
         // Logging
         implementation("org.slf4j:slf4j-api:1.7.32")
         implementation("ch.qos.logback:logback-classic:1.2.6")
-        implementation("io.github.microutils:kotlin-logging:2.0.11")
+        implementation("io.github.microutils:kotlin-logging:2.1.21")
 
         // ReactiveX
         implementation("io.reactivex:rxjava:1.3.8")
@@ -92,7 +92,7 @@ configure(projects) {
         implementation("com.jakewharton.rxrelay:rxrelay:1.2.0")
 
         // dependency both in AndroidCompat and extensions, version locked by Tachiyomi app/extensions
-        implementation("org.jsoup:jsoup:1.14.2")
+        implementation("org.jsoup:jsoup:1.14.3")
 
         // dependency of :AndroidCompat:Config
         implementation("com.typesafe:config:1.4.1")
