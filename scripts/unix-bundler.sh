6a795680b6c062fdf8b29e6890d143e3b63a79b4
Mahor <mahor1221@pm.me>
2022-06-16 15:52:53 +0430

    Add linux-all.tar.gz

--- scripts/unix-bundler.sh
+++ scripts/unix-bundler.sh
@@ -10,7 +10,9 @@ electron_version="v14.0.0"
 
 arch="$1"
 os="$(echo $arch | cut -d '-' -f1)"
-if [ "$arch" = "linux-x64" ]; then
+if [ "$arch" = "linux-all" ]; then
+  # continue
+elif [ "$arch" = "linux-x64" ]; then
   jre="OpenJDK8U-jre_x64_linux_hotspot_8u302b08.tar.gz"
   jre_release="jdk8u302-b08"
   jre_url="https://github.com/adoptium/temurin8-binaries/releases/download/$jre_release/$jre"
@@ -40,25 +42,30 @@ release_name="$(echo $jar_name | sed 's/.jar//')-$arch"
 mkdir "$release_name"
 
 # download jre and electron
-echo "Dealing with jre..."
-if [ ! -f "$jre" ]; then
-  curl -L "$jre_url" -o "$jre"
-fi
-tar xvf "$jre"
-mv "$jre_dir" "$release_name/jre"
+if [ "$arch" != "linux-all" ]; then
+  echo "Dealing with jre..."
+  if [ ! -f "$jre" ]; then
+    curl -L "$jre_url" -o "$jre"
+  fi
+  tar xvf "$jre"
+  mv "$jre_dir" "$release_name/jre"
 
-echo "Dealing with electron"
-if [ ! -f "$electron" ]; then
-  curl -L "https://github.com/electron/electron/releases/download/$electron_version/$electron" -o "$electron"
+  echo "Dealing with electron"
+  if [ ! -f "$electron" ]; then
+    curl -L "https://github.com/electron/electron/releases/download/$electron_version/$electron" -o "$electron"
+  fi
+  unzip "$electron" -d "$release_name/electron"
 fi
-unzip "$electron" -d "$release_name/electron"
 
 # copy artifacts
-cp "$jar $release_name/Tachidesk-Server.jar"
+cp "$jar $release_name/tachidesk-server.jar"
 if [ "$os" = "linux" ]; then
   cp "resources/tachidesk-server-browser-launcher.sh" "$release_name"
   cp "resources/tachidesk-server-debug-launcher.sh" "$release_name"
   cp "resources/tachidesk-server-electron-launcher.sh" "$release_name"
+  cp "resources/tachidesk-server.desktop" "$release_name"
+  cp "../server/src/main/resources/icon/faviconlogo.png" "$release_name/tachidesk-server.png"
+  cp "resources/systemd"/* "$release_name" 
 elif [ "$os" = "macOS" ]; then
   cp "resources/Tachidesk Browser Launcher.command" "$release_name"
   cp "resources/Tachidesk Debug Launcher.command" "$release_name"
