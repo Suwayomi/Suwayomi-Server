7931bb4a692d54649be15a6aea769a7a5d798a1e
Mahor <mahor1221@pm.me>
2022-08-04 13:48:51 +0430

    Tidy up bundler script

--- scripts/bundler.sh
+++ scripts/bundler.sh
@@ -8,12 +8,11 @@
 
 main() {
   POSITIONAL_ARGS=()
-  DOWNLOAD_ONLY=false
   OUTPUT_DIR=.
   while [ "$#" -gt 0 ]; do
     case "$1" in
       -o|--output-dir)
-        OUTPUT_DIR="$(readlink -e "$2")"
+        OUTPUT_DIR="$(readlink -e "$2" || exit 1)"
         shift
         shift
         ;;
@@ -23,76 +22,93 @@ main() {
         ;;
     esac
   done
-  set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+  # restore positional parameters
+  set -- "${POSITIONAL_ARGS[@]}"
 
   OS="$1"
-  jar="$(ls server/build/*.jar | tail -n1)"
-  release_name="$(echo "${jar%.*}" | sed 's/.jar//' | xargs basename)-$OS"
-  release_version="$(tmp="${jar%-*}"; echo "${tmp##*-}" | tr -d v)"
-  #local release_revision_number="$(tmp="${jar%.*}" && echo "${tmp##*-}" | tr -d r)"
-  electron_version="v14.0.0"
+  JAR="$(ls server/build/*.jar | tail -n1)"
+  RELEASE_NAME="$(echo "${JAR%.*}" | xargs basename)-$OS"
+  RELEASE_VERSION="$(tmp="${JAR%-*}"; echo "${tmp##*-}" | tr -d v)"
+  #RELEASE_REVISION_NUMBER="$(tmp="${JAR%.*}" && echo "${tmp##*-}" | tr -d r)"
+  local electron_version="v14.0.0"
 
   # clean temporary directory on function return
-  trap "rm -rf $release_name/" RETURN
-  mkdir "$release_name/"
+  trap "rm -rf $RELEASE_NAME/" RETURN
+  mkdir "$RELEASE_NAME/"
 
   case "$OS" in
     debian-all)
+      RELEASE="$RELEASE_NAME.deb"
       make_deb_package
       move_release_to_output_dir
       ;;
     linux-all)
-      make_linux_bundle
+      RELEASE="$RELEASE_NAME.tar.gz"
+      cp "$JAR" "$RELEASE_NAME/tachidesk-server.jar"
+      copy_linux_package_assets_to "$RELEASE_NAME/"
+      tar -I "gzip -9" -cvf "$RELEASE" "$RELEASE_NAME/"
       move_release_to_output_dir
       ;;
     linux-x64)
-      jre="OpenJDK8U-jre_x64_linux_hotspot_8u302b08.tar.gz"
-      jre_url="https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/$jre"
-      electron="electron-$electron_version-linux-x64.zip"
-      electron_url="https://github.com/electron/electron/releases/download/$electron_version/$electron"
+      JRE="OpenJDK8U-jre_x64_linux_hotspot_8u302b08.tar.gz"
+      JRE_URL="https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/$JRE"
+      ELECTRON="electron-$electron_version-linux-x64.zip"
+      ELECTRON_URL="https://github.com/electron/electron/releases/download/$electron_version/$ELECTRON"
       download_jre_and_electron
+
+      RELEASE="$RELEASE_NAME.tar.gz"
       make_linux_bundle
       move_release_to_output_dir
       ;;
     macOS-x64)
-      jre="OpenJDK8U-jre_x64_mac_hotspot_8u302b08.tar.gz"
-      jre_url="https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/$jre"
-      electron="electron-$electron_version-darwin-x64.zip"
-      electron_url="https://github.com/electron/electron/releases/download/$electron_version/$electron"
+      JRE="OpenJDK8U-jre_x64_mac_hotspot_8u302b08.tar.gz"
+      JRE_URL="https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/$JRE"
+      ELECTRON="electron-$electron_version-darwin-x64.zip"
+      ELECTRON_URL="https://github.com/electron/electron/releases/download/$electron_version/$ELECTRON"
       download_jre_and_electron
+
+      RELEASE="$RELEASE_NAME.zip"
       make_macos_bundle
       move_release_to_output_dir
       ;;
     macOS-arm64)
-      jre="zulu8.56.0.23-ca-jre8.0.302-macosx_aarch64.tar.gz"
-      jre_url="https://cdn.azul.com/zulu/bin/$jre"
-      electron="electron-$electron_version-darwin-arm64.zip"
-      electron_url="https://github.com/electron/electron/releases/download/$electron_version/$electron"
+      JRE="zulu8.56.0.23-ca-jre8.0.302-macosx_aarch64.tar.gz"
+      JRE_URL="https://cdn.azul.com/zulu/bin/$JRE"
+      ELECTRON="electron-$electron_version-darwin-arm64.zip"
+      ELECTRON_URL="https://github.com/electron/electron/releases/download/$electron_version/$ELECTRON"
       download_jre_and_electron
+
+      RELEASE="$RELEASE_NAME.zip"
       make_macos_bundle
       move_release_to_output_dir
       ;;
     windows-x86)
-      jre="OpenJDK8U-jre_x86-32_windows_hotspot_8u292b10.zip"
-      jre_url="https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/$jre"
-      electron="electron-$electron_version-win32-ia32.zip"
-      electron_url="https://github.com/electron/electron/releases/download/$electron_version/$electron"
+      JRE="OpenJDK8U-jre_x86-32_windows_hotspot_8u292b10.zip"
+      JRE_URL="https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/$JRE"
+      ELECTRON="electron-$electron_version-win32-ia32.zip"
+      ELECTRON_URL="https://github.com/electron/electron/releases/download/$electron_version/$ELECTRON"
       download_jre_and_electron
+
+      RELEASE="$RELEASE_NAME.zip"
       make_windows_bundle
       move_release_to_output_dir
 
+      RELEASE="$RELEASE_NAME.msi"
       make_windows_package
       move_release_to_output_dir
       ;;
     windows-x64)
-      jre="OpenJDK8U-jre_x64_windows_hotspot_8u302b08.zip"
-      jre_url="https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/$jre"
-      electron="electron-$electron_version-win32-x64.zip"
-      electron_url="https://github.com/electron/electron/releases/download/$electron_version/$electron"
+      JRE="OpenJDK8U-jre_x64_windows_hotspot_8u302b08.zip"
+      JRE_URL="https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/$JRE"
+      ELECTRON="electron-$electron_version-win32-x64.zip"
+      ELECTRON_URL="https://github.com/electron/electron/releases/download/$electron_version/$ELECTRON"
       download_jre_and_electron
+
+      RELEASE="$RELEASE_NAME.zip"
       make_windows_bundle
       move_release_to_output_dir
 
+      RELEASE="$RELEASE_NAME.msi"
       make_windows_package
       move_release_to_output_dir
       ;;
@@ -104,86 +120,96 @@ main() {
 
 move_release_to_output_dir() {
    # clean up from possible previous runs
-   if [ -f "$OUTPUT_DIR/$release" ]; then
-     rm "$OUTPUT_DIR/$release"
+   if [ -f "$OUTPUT_DIR/$RELEASE" ]; then
+     rm "$OUTPUT_DIR/$RELEASE"
    fi
-   mv "$release" "$OUTPUT_DIR/"
+   mv "$RELEASE" "$OUTPUT_DIR/"
 }
 
 download_jre_and_electron() {
-  if [ ! -f "$jre" ]; then
-    curl -L "$jre_url" -o "$jre"
+  if [ ! -f "$JRE" ]; then
+    curl -L "$JRE_URL" -o "$JRE"
   fi
-  if [ ! -f "$electron" ]; then
-    curl -L "$electron_url" -o "$electron"
+  if [ ! -f "$ELECTRON" ]; then
+    curl -L "$ELECTRON_URL" -o "$ELECTRON"
   fi
 
-  mkdir -p "$release_name/jre/"
-  local ext="${jre##*.}"
+  mkdir -p "$RELEASE_NAME/jre/"
+  local ext="${JRE##*.}"
+  local jre_dir
   if [ "$ext" = "zip" ]; then
-    jre_dir="$(unzip "$jre" | sed -n '2p' | cut -d: -f2 | xargs basename)"
-    mv "$jre_dir" "$release_name/jre"
+    jre_dir="$(unzip "$JRE" | sed -n '2p' | cut -d: -f2 | xargs basename)"
+    mv "$jre_dir" "$RELEASE_NAME/jre"
   else
     # --strip-components=1: untar an archive without the root folder
-    tar xvf "$jre" --strip-components=1 -C "$release_name/jre/"
+    tar xvf "$JRE" --strip-components=1 -C "$RELEASE_NAME/jre/"
   fi
-  unzip "$electron" -d "$release_name/electron/"
+  unzip "$ELECTRON" -d "$RELEASE_NAME/electron/"
+}
+
+copy_linux_package_assets_to() {
+  local output_dir
+  output_dir="$(readlink -e "$1" || exit 1)"
+
+  cp "scripts/resources/tachidesk-server-browser-launcher.sh" "$output_dir/"
+  cp "scripts/resources/tachidesk-server-debug-launcher.sh" "$output_dir/"
+  cp "scripts/resources/tachidesk-server-electron-launcher.sh" "$output_dir/"
+  cp "scripts/resources/tachidesk-server.desktop" "$output_dir/"
+  cp "scripts/resources/systemd"/* "$output_dir/"
+  cp "server/src/main/resources/icon/faviconlogo.png" \
+    "$output_dir/tachidesk-server.png"
 }
 
 make_linux_bundle() {
-  cp "$jar" "$release_name/tachidesk-server.jar"
-  cp "scripts/resources/tachidesk-server-browser-launcher.sh" "$release_name/"
-  cp "scripts/resources/tachidesk-server-debug-launcher.sh" "$release_name/"
-  cp "scripts/resources/tachidesk-server-electron-launcher.sh" "$release_name/"
-  cp "scripts/resources/tachidesk-server.desktop" "$release_name/"
-  cp "server/src/main/resources/icon/faviconlogo.png" \
-    "$release_name/tachidesk-server.png"
-  cp "scripts/resources/systemd"/* "$release_name/"
+  cp "$JAR" "$RELEASE_NAME/tachidesk-server.jar"
+  cp "scripts/resources/tachidesk-server-browser-launcher.sh" "$RELEASE_NAME/"
+  cp "scripts/resources/tachidesk-server-debug-launcher.sh" "$RELEASE_NAME/"
+  cp "scripts/resources/tachidesk-server-electron-launcher.sh" "$RELEASE_NAME/"
 
-  release="$release_name.tar.gz"
-  tar -I "gzip -9" -cvf "$release" "$release_name/"
+  tar -I "gzip -9" -cvf "$RELEASE" "$RELEASE_NAME/"
 }
 
 make_macos_bundle() {
-  cp "$jar" "$release_name/Tachidesk.jar"
-  cp "scripts/resources/Tachidesk Browser Launcher.command" "$release_name/"
-  cp "scripts/resources/Tachidesk Debug Launcher.command" "$release_name/"
-  cp "scripts/resources/Tachidesk Electron Launcher.command" "$release_name/"
+  cp "$JAR" "$RELEASE_NAME/Tachidesk.jar"
+  cp "scripts/resources/Tachidesk Browser Launcher.command" "$RELEASE_NAME/"
+  cp "scripts/resources/Tachidesk Debug Launcher.command" "$RELEASE_NAME/"
+  cp "scripts/resources/Tachidesk Electron Launcher.command" "$RELEASE_NAME/"
 
-  release="$release_name.zip"
-  zip -9 -r "$release" "$release_name/"
+  zip -9 -r "$RELEASE" "$RELEASE_NAME/"
 }
 
 # https://wiki.debian.org/SimplePackagingTutorial
 # https://www.debian.org/doc/manuals/packaging-tutorial/packaging-tutorial.pdf
 make_deb_package() {
-  make_linux_bundle "$release_name" "$jar"
-  cp -r "scripts/resources/deb/" "$release_name/debian/"
-  sed -i "s/\$pkgver/$release_version/" "$release_name/debian/changelog"
-  sed -i "s/\$pkgrel/1/"                "$release_name/debian/changelog"
-
-  # clean temporary directory on function return
-  local temp_dir="$(mktemp -d)"
+  local temp_dir
+  temp_dir="$(mktemp -d)"
   trap "rm -rf $temp_dir" RETURN
 
-  #behind $release_version is underscore "_"
-  local source_dir="tachidesk-server-$release_version"
-  local upstream_source="tachidesk-server_$release_version.orig.tar.gz"
-  mv "$release_name/"       "$temp_dir/$source_dir/"
-  mv "$release_name.tar.gz" "$temp_dir/$upstream_source"
+  cp "$JAR" "$RELEASE_NAME/tachidesk-server.jar"
+  tar -I "gzip" -cvf "$RELEASE_NAME.tar.gz" "$RELEASE_NAME/"
+  #behind $RELEASE_VERSION is underscore "_"
+  local upstream_source="tachidesk-server_$RELEASE_VERSION.orig.tar.gz"
+  mv "$RELEASE_NAME.tar.gz" "$temp_dir/$upstream_source"
+
+  cp -r "scripts/resources/deb/" "$RELEASE_NAME/debian/"
+  copy_linux_package_assets_to "$RELEASE_NAME/debian/"
+  sed -i "s/\$pkgver/$RELEASE_VERSION/" "$RELEASE_NAME/debian/changelog"
+  sed -i "s/\$pkgrel/1/"                "$RELEASE_NAME/debian/changelog"
+  #behind $RELEASE_VERSION is hyphen "-"
+  local source_dir="tachidesk-server-$RELEASE_VERSION"
+  mv "$RELEASE_NAME/" "$temp_dir/$source_dir/"
 
   sudo apt install devscripts build-essential dh-exec
-  cd "$temp_dir/$source_dir"
+  cd "$temp_dir/$source_dir/"
   dpkg-buildpackage --no-sign --build=all
   cd -
 
-  local deb="tachidesk-server_$release_version-1_all.deb"
-  release="$release_name.deb"
-  mv "$temp_dir/$deb" "$release"
+  local deb="tachidesk-server_$RELEASE_VERSION-1_all.deb"
+  mv "$temp_dir/$deb" "$RELEASE"
 }
 
 make_windows_bundle() {
-  ## I'll disable this section until someone find a solution to this error:
+  ## I disabled this section until someone find a solution to this error:
   ##E: Unable to correct problems, you have held broken packages.
   ##./bundler.sh: line 250: wine: command not found
 
@@ -207,17 +233,16 @@ make_windows_bundle() {
     #curl -L "$rcedit_url" -o "$rcedit"
   #fi
 
-  local icon="server/src/main/resources/icon/faviconlogo.ico"
-  #WINEARCH=win32 wine "$rcedit" "$release_name/electron/electron.exe" \
+  #local icon="server/src/main/resources/icon/faviconlogo.ico"
+  #WINEARCH=win32 wine "$rcedit" "$RELEASE_NAME/electron/electron.exe" \
   #    --set-icon "$icon"
 
-  cp "$jar" "$release_name/Tachidesk.jar"
-  cp "scripts/resources/Tachidesk Browser Launcher.bat" "$release_name"
-  cp "scripts/resources/Tachidesk Debug Launcher.bat" "$release_name"
-  cp "scripts/resources/Tachidesk Electron Launcher.bat" "$release_name"
+  cp "$JAR" "$RELEASE_NAME/Tachidesk.jar"
+  cp "scripts/resources/Tachidesk Browser Launcher.bat" "$RELEASE_NAME"
+  cp "scripts/resources/Tachidesk Debug Launcher.bat" "$RELEASE_NAME"
+  cp "scripts/resources/Tachidesk Electron Launcher.bat" "$RELEASE_NAME"
 
-  release="$release_name.zip"
-  zip -9 -r "$release" "$release_name"
+  zip -9 -r "$RELEASE" "$RELEASE_NAME"
 }
 
 make_windows_package() {
@@ -225,20 +250,19 @@ make_windows_package() {
     sudo apt install -y wixl
   fi
 
-  find "$release_name/jre" \
-  | wixl-heat --var var.SourceDir -p "$release_name/" \
-    --directory-ref jre --component-group jre >"$release_name/jre.wxs"
-  find "$release_name/electron" \
-  | wixl-heat --var var.SourceDir -p "$release_name/" \
-    --directory-ref electron --component-group electron >"$release_name/electron.wxs"
+  find "$RELEASE_NAME/jre" \
+  | wixl-heat --var var.SourceDir -p "$RELEASE_NAME/" \
+    --directory-ref jre --component-group jre >"$RELEASE_NAME/jre.wxs"
+  find "$RELEASE_NAME/electron" \
+  | wixl-heat --var var.SourceDir -p "$RELEASE_NAME/" \
+    --directory-ref electron --component-group electron >"$RELEASE_NAME/electron.wxs"
 
   local icon="server/src/main/resources/icon/faviconlogo.ico"
   local arch=${OS##*-}
-  release="$release_name.msi"
 
-  wixl -D ProductVersion="$release_version" -D SourceDir="$release_name" \
+  wixl -D ProductVersion="$RELEASE_VERSION" -D SourceDir="$RELEASE_NAME" \
     -D Icon="$icon" --arch "$arch" "scripts/resources/msi/tachidesk-server-$arch.wxs" \
-    "$release_name/jre.wxs" "$release_name/electron.wxs" -o "$release"
+    "$RELEASE_NAME/jre.wxs" "$RELEASE_NAME/electron.wxs" -o "$RELEASE"
 }
 
 # Error handler
@@ -252,17 +276,18 @@ make_windows_package() {
 # and http://mywiki.wooledge.org/BashFAO/105
 set -euo pipefail
 error() {
-    local parent_lineno="$1"
-    local message="$2"
-    local code="${3:-1}"
-    if [ -z "$message" ]; then
-        echo "$0: line $parent_lineno: exiting with status $code"
-    else
-        echo "$0: line $parent_lineno: $message: exiting with status $code"
-    fi
-    exit "$code"
+  local parent_lineno="$1"
+  local message="$2"
+  local code="${3:-1}"
+  if [ -z "$message" ]; then
+      echo "$0: line $parent_lineno: exiting with status $code"
+  else
+      echo "$0: line $parent_lineno: $message: exiting with status $code"
+  fi
+  exit "$code"
 }
 trap 'error $LINENO ""' ERR
 
 main "$@"; exit
+
 
