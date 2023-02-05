f91d85dc2d6a948391406107d6dc0a395ef4e153
akxer <>
2023-02-05 12:26:36 +0530

    Fixes Dex2Jar and dorkbox dependency issues

--- server/src/main/kotlin/suwayomi/tachidesk/server/util/Browser.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/server/util/Browser.kt
@@ -7,7 +7,7 @@ package suwayomi.tachidesk.server.util
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import dorkbox.util.Desktop
+import dorkbox.desktop.Desktop
 import suwayomi.tachidesk.server.serverConfig
 
 object Browser {
