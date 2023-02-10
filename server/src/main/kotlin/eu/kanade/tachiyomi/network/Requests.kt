578733608e75aad79286e814ccf943667d75346c
Syer10 <syer10@users.noreply.github.com>
2023-02-10 13:05:03 -0500

    Support extensions lib 1.4

--- server/src/main/kotlin/eu/kanade/tachiyomi/network/Requests.kt
+++ server/src/main/kotlin/eu/kanade/tachiyomi/network/Requests.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.network
 import okhttp3.CacheControl
 import okhttp3.FormBody
 import okhttp3.Headers
+import okhttp3.HttpUrl
 import okhttp3.Request
 import okhttp3.RequestBody
 import java.util.concurrent.TimeUnit.MINUTES
@@ -15,6 +16,21 @@ fun GET(
     url: String,
     headers: Headers = DEFAULT_HEADERS,
     cache: CacheControl = DEFAULT_CACHE_CONTROL
+): Request {
+    return Request.Builder()
+        .url(url)
+        .headers(headers)
+        .cacheControl(cache)
+        .build()
+}
+
+/**
+ * @since extensions-lib 1.4
+ */
+fun GET(
+    url: HttpUrl,
+    headers: Headers = DEFAULT_HEADERS,
+    cache: CacheControl = DEFAULT_CACHE_CONTROL,
 ): Request {
     return Request.Builder()
         .url(url)
