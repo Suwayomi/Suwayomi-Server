42d1c02f8d4b63790d019241e21763270e75cd2e
Syer10 <syer10@users.noreply.github.com>
2023-05-29 17:26:33 -0400

    Fix cascade migration

--- server/src/main/kotlin/suwayomi/tachidesk/server/database/migration/M0028_AddCascade.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/server/database/migration/M0028_AddCascade.kt
@@ -6,34 +6,42 @@ import de.neonew.exposed.migrations.helpers.SQLMigration
 class M0028_AddCascade : SQLMigration() {
     override val sql: String = """
         alter table CATEGORYMANGA
-            drop constraint FK_CATEGORYMANGA_CATEGORY_ID;
+            drop constraint if exists FK_CATEGORYMANGA_CATEGORY__ID;
+        alter table CATEGORYMANGA
+            drop constraint if exists FK_CATEGORYMANGA_CATEGORY_ID;
         
         alter table CATEGORYMANGA
-            add constraint FK_CATEGORYMANGA_CATEGORY_ID
+            add constraint FK_CATEGORYMANGA_CATEGORY__ID
                 foreign key (CATEGORY) references CATEGORY
                     on delete cascade;
         
         alter table CATEGORYMANGA
-            drop constraint FK_CATEGORYMANGA_MANGA_ID;
+            drop constraint if exists FK_CATEGORYMANGA_MANGA__ID;
+        alter table CATEGORYMANGA
+            drop constraint if exists FK_CATEGORYMANGA_MANGA_ID;
         
         alter table CATEGORYMANGA
-            add constraint FK_CATEGORYMANGA_MANGA_ID
+            add constraint FK_CATEGORYMANGA_MANGA__ID
                 foreign key (MANGA) references MANGA
                     on delete cascade;
         
         alter table CHAPTER
-            drop constraint FK_CHAPTER_MANGA_ID;
+            drop constraint if exists FK_CHAPTER_MANGA__ID;
+        alter table CHAPTER
+            drop constraint if exists FK_CHAPTER_MANGA_ID;
         
         alter table CHAPTER
-            add constraint FK_CHAPTER_MANGA_ID
+            add constraint FK_CHAPTER_MANGA__ID
                 foreign key (MANGA) references MANGA
                     on delete cascade;
         
         alter table PAGE
-            drop constraint FK_PAGE_CHAPTER_ID;
+            drop constraint if exists FK_PAGE_CHAPTER__ID;
+        alter table PAGE
+            drop constraint if exists FK_PAGE_CHAPTER_ID;
         
         alter table PAGE
-            add constraint FK_PAGE_CHAPTER_ID
+            add constraint FK_PAGE_CHAPTER__ID
                 foreign key (CHAPTER) references CHAPTER
                     on delete cascade;
     """.trimIndent()
