65937c81fc4d91391a0f38be539abe40a6589f04
schroda <50052685+schroda@users.noreply.github.com>
2023-07-20 23:46:02 +0200

    Setup "logback" to write to file
    To be able to dynamically set the log file save location, logback has to be setup via code instead of a config file

--- AndroidCompat/Config/src/main/java/xyz/nulldev/ts/config/Logging.kt
+++ AndroidCompat/Config/src/main/java/xyz/nulldev/ts/config/Logging.kt
@@ -8,12 +8,65 @@ package xyz.nulldev.ts.config
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy
+import ch.qos.logback.core.util.FileSize
 import com.typesafe.config.Config
 import mu.KotlinLogging
 import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+private fun createRollingFileAppender(logContext: LoggerContext, logDirPath: String): RollingFileAppender<ILoggingEvent> {
+    val logFilename = "application"
+
+    val logEncoder = PatternLayoutEncoder().apply {
+        pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS} [%thread] %-5level %logger - %msg%n"
+        context = logContext
+        start()
+    }
+
+    val appender = RollingFileAppender<ILoggingEvent>().apply {
+        name = "FILE"
+        context = logContext
+        encoder = logEncoder
+        file = "$logDirPath/$logFilename.log"
+    }
+
+    val rollingPolicy = SizeAndTimeBasedRollingPolicy<ILoggingEvent>().apply {
+        context = logContext
+        setParent(appender)
+        fileNamePattern = "$logDirPath/${logFilename}_%d{yyyy-MM-dd}_%i.log.gz"
+        setMaxFileSize(FileSize.valueOf("10mb"))
+        maxHistory = 14
+        setTotalSizeCap(FileSize.valueOf("1gb"))
+        start()
+    }
+
+    appender.rollingPolicy = rollingPolicy
+    appender.start()
+
+    return appender
+}
+
+private fun getBaseLogger(): ch.qos.logback.classic.Logger {
+    return (KotlinLogging.logger(Logger.ROOT_LOGGER_NAME).underlyingLogger as ch.qos.logback.classic.Logger)
+}
+
+fun initLoggerConfig(appRootPath: String) {
+    val context = LoggerFactory.getILoggerFactory() as LoggerContext
+    val logger = getBaseLogger()
+    // logback logs to the console by default (at least when adding a console appender logs in the console are duplicated)
+    logger.addAppender(createRollingFileAppender(context, "$appRootPath/logs"))
+
+    // set "kotlin exposed" log level
+    context.getLogger("Exposed").level = Level.ERROR
+}
 
 fun setLogLevel(level: Level) {
-    (KotlinLogging.logger(Logger.ROOT_LOGGER_NAME).underlyingLogger as ch.qos.logback.classic.Logger).level = level
+    getBaseLogger().level = level
 }
 
 fun debugLogsEnabled(config: Config) =
