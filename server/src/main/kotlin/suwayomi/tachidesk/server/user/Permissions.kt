30b47147162cd2653c6125e5b141ee1d98e94b96
Syer10 <syer10@users.noreply.github.com>
2023-07-29 20:14:41 -0400

    Forgot to add new files

---
+++ server/src/main/kotlin/suwayomi/tachidesk/server/user/Permissions.kt
@@ -0,0 +1,9 @@
+package suwayomi.tachidesk.server.user
+
+enum class Permissions {
+    INSTALL_EXTENSIONS,
+    INSTALL_UNTRUSTED_EXTENSIONS,
+    UNINSTALL_EXTENSIONS,
+    DOWNLOAD_CHAPTERS,
+    DELETE_DOWNLOADS
+}
---
+++ server/src/main/kotlin/suwayomi/tachidesk/server/user/UserType.kt
@@ -0,0 +1,37 @@
+package suwayomi.tachidesk.server.user
+
+sealed class UserType {
+    class Admin(val id: Int) : UserType()
+
+    class User(
+        val id: Int,
+        val permissions: List<Permissions>
+    ) : UserType()
+
+    object Visitor : UserType()
+}
+
+fun UserType.requireUser(): Int {
+    return when (this) {
+        is UserType.Admin -> id
+        is UserType.User -> id
+        UserType.Visitor -> throw UnauthorizedException()
+    }
+}
+
+fun UserType.requirePermissions(vararg permissions: Permissions) {
+    when (this) {
+        is UserType.Admin -> Unit
+        is UserType.User -> {
+            val userPermissions = this.permissions
+            if (!permissions.all { it in userPermissions }) {
+                throw ForbiddenException()
+            }
+        }
+        UserType.Visitor -> throw UnauthorizedException()
+    }
+}
+
+class UnauthorizedException : IllegalStateException("Unauthorized")
+
+class ForbiddenException : IllegalStateException("Forbidden")
