2e666c0979f5dae52085ee44d5bf4dc3e1dfc2fd
Syer10 <syer10@users.noreply.github.com>
2023-08-06 12:37:57 -0400

    Remove Playwright

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -56,9 +56,6 @@ dependencies {
     implementation(libs.commonscompress)
     implementation(libs.junrar)
 
-    // CloudflareInterceptor
-    implementation(libs.playwright)
-
     // AES/CBC/PKCS7Padding Cypher provider for zh.copymanga
     implementation(libs.bouncycastle)
 
