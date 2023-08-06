74416e0223a6194d3ef0a45951fba995a54572c9
Syer10 <syer10@users.noreply.github.com>
2023-08-06 12:24:10 -0400

    Sync cookies

--- server/src/main/kotlin/suwayomi/tachidesk/global/impl/WebView.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/global/impl/WebView.kt
@@ -1,12 +1,16 @@
 package suwayomi.tachidesk.global.impl
 
+import eu.kanade.tachiyomi.network.NetworkHelper
 import eu.kanade.tachiyomi.source.online.HttpSource
 import io.javalin.websocket.WsContext
 import io.javalin.websocket.WsMessageContext
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
@@ -18,8 +22,10 @@ import kotlinx.serialization.json.longOrNull
 import org.eclipse.jetty.websocket.api.CloseStatus
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.openqa.selenium.Cookie
 import org.openqa.selenium.Dimension
 import org.openqa.selenium.Keys
+import org.openqa.selenium.NoSuchSessionException
 import org.openqa.selenium.OutputType
 import org.openqa.selenium.TakesScreenshot
 import org.openqa.selenium.WebDriver
@@ -32,7 +38,12 @@ import suwayomi.tachidesk.manga.model.table.ChapterTable
 import suwayomi.tachidesk.manga.model.table.MangaTable
 import uy.kohesive.injekt.injectLazy
 import java.io.Closeable
+import java.net.HttpCookie
+import java.net.URI
+import java.util.Date
 import java.util.concurrent.Executors
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 object WebView : Websocket<String>() {
     val json: Json by injectLazy()
@@ -79,6 +90,8 @@ class SeleniumScreenshotServer : Closeable {
     companion object {
         const val width = 1200
         const val height = 800
+
+        private val networkHelper: NetworkHelper by injectLazy()
     }
 
     private val driver: WebDriver = run {
@@ -88,8 +101,9 @@ class SeleniumScreenshotServer : Closeable {
     }
     init {
         driver.manage().window().size = Dimension(width, height)
-        driver.get("https://google.com")
+        driver.navigate().to("https://google.com")
     }
+    private val job = SupervisorJob()
     private val executor = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
 
     init {
@@ -109,6 +123,12 @@ class SeleniumScreenshotServer : Closeable {
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
+            }
+        }
+        GlobalScope.launch(job) {
+            while (true) {
+                delay(5.seconds)
+                flush()
             }
         }
     }
@@ -141,22 +161,30 @@ class SeleniumScreenshotServer : Closeable {
             "loadsource" -> {
                 val sourceId = jsonObject["source"]!!.jsonPrimitive.longOrNull ?: return
                 val source = getCatalogueSourceOrNull(sourceId) as? HttpSource ?: return
-                driver.get(source.baseUrl)
+                driver.navigate().to(source.baseUrl)
+                addCookiesFor(driver.currentUrl)
+                driver.navigate().refresh()
             }
             "loadmanga" -> {
                 val mangaId = jsonObject["manga"]!!.jsonPrimitive.intOrNull ?: return
                 val manga = transaction { MangaTable.select { MangaTable.id eq mangaId }.firstOrNull() } ?: return
-                driver.get(manga[MangaTable.realUrl] ?: return)
+                driver.navigate().to(manga[MangaTable.realUrl] ?: return)
+                addCookiesFor(driver.currentUrl)
+                driver.navigate().refresh()
             }
             "loadchapter" -> {
                 val chapterId = jsonObject["chapter"]!!.jsonPrimitive.intOrNull ?: return
                 val chapter = transaction { ChapterTable.select { ChapterTable.id eq chapterId }.firstOrNull() } ?: return
-                driver.get(chapter[ChapterTable.realUrl] ?: return)
+                driver.navigate().to(chapter[ChapterTable.realUrl] ?: return)
+                addCookiesFor(driver.currentUrl)
+                driver.navigate().refresh()
             }
         }
     }
 
     override fun close() {
+        job.cancel()
+        flush()
         executor.cancel()
         try {
             driver.quit()
@@ -165,6 +193,45 @@ class SeleniumScreenshotServer : Closeable {
         try {
             driver.close()
         } catch (_: Exception) {
+        }
+    }
+
+    private fun flush() {
+        driver.manage().cookies.forEach {
+            networkHelper.cookieStore.add(
+                URI("http://" + it.domain),
+                HttpCookie(it.name, it.value).apply {
+                    path = it.path
+                    domain = it.domain
+                    maxAge = if (it.expiry == null) {
+                        -1
+                    } else {
+                        (it.expiry.time.milliseconds - System.currentTimeMillis().milliseconds).inWholeSeconds
+                    }
+                    isHttpOnly = it.isHttpOnly
+                    secure = it.isSecure
+                }
+            )
+        }
+    }
+
+    private fun addCookiesFor(uri: String) {
+        networkHelper.cookieStore.get(URI(uri)).forEach {
+            driver.manage().addCookie(
+                Cookie(
+                    it.name,
+                    it.value,
+                    it.domain,
+                    it.path,
+                    if (it.maxAge == -1L) {
+                        null
+                    } else {
+                        Date(System.currentTimeMillis() + it.maxAge.seconds.inWholeMilliseconds)
+                    },
+                    it.secure,
+                    it.isHttpOnly
+                )
+            )
         }
     }
 }
