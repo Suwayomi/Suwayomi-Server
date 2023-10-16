0ba6c88d69fa8a80cfb077baf56ef7bcf5d6c944
schroda <50052685+schroda@users.noreply.github.com>
2023-10-16 02:14:20 +0200

    Fix/graphql mangas query genre based filtering (#713)
    * Filter mangas based on each genre of the genre condition
    
    Genres are stored as a comma separated string in the database.
    Thus, unless the correct string was passed in the condition, no manga would match the condition.
    
    * Query mangas based on genre filter

--- server/src/main/kotlin/suwayomi/tachidesk/graphql/queries/MangaQuery.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/graphql/queries/MangaQuery.kt
@@ -122,7 +122,7 @@ class MangaQuery {
             opAnd.eq(artist, MangaTable.artist)
             opAnd.eq(author, MangaTable.author)
             opAnd.eq(description, MangaTable.description)
-            opAnd.eq(genre?.joinToString(), MangaTable.genre)
+            genre?.forEach { opAnd.like("%$it%", MangaTable.genre) }
             opAnd.eq(status?.value, MangaTable.status)
             opAnd.eq(inLibrary, MangaTable.inLibrary)
             opAnd.eq(inLibraryAt, MangaTable.inLibraryAt)
@@ -172,7 +172,7 @@ class MangaQuery {
         val artist: StringFilter? = null,
         val author: StringFilter? = null,
         val description: StringFilter? = null,
-        // val genre: List<String>? = null, // todo
+        val genre: StringFilter? = null,
         val status: MangaStatusFilter? = null,
         val inLibrary: BooleanFilter? = null,
         val inLibraryAt: LongFilter? = null,
@@ -194,6 +194,7 @@ class MangaQuery {
                 andFilterWithCompareString(MangaTable.artist, artist),
                 andFilterWithCompareString(MangaTable.author, author),
                 andFilterWithCompareString(MangaTable.description, description),
+                andFilterWithCompareString(MangaTable.genre, genre),
                 andFilterWithCompare(MangaTable.status, status?.asIntFilter()),
                 andFilterWithCompare(MangaTable.inLibrary, inLibrary),
                 andFilterWithCompare(MangaTable.inLibraryAt, inLibraryAt),
