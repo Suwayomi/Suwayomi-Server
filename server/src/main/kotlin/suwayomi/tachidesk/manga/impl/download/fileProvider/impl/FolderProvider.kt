75901f6a13a8f7cb55c2c2c915e2b10af2c94ae8
schroda <50052685+schroda@users.noreply.github.com>
2023-11-19 12:30:13 +0100

    Cleanup parent folders when deleting downloaded chapters
    Currently, the download folder never gets cleaned up which leads to having a lot of empty folders (sources, mangas folders)

--- server/src/main/kotlin/suwayomi/tachidesk/manga/impl/download/fileProvider/impl/FolderProvider.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/manga/impl/download/fileProvider/impl/FolderProvider.kt
@@ -1,13 +1,20 @@
 package suwayomi.tachidesk.manga.impl.download.fileProvider.impl
 
 import kotlinx.coroutines.CoroutineScope
+import org.kodein.di.DI
+import org.kodein.di.conf.global
+import org.kodein.di.instance
 import suwayomi.tachidesk.manga.impl.download.fileProvider.ChaptersFilesProvider
 import suwayomi.tachidesk.manga.impl.download.model.DownloadChapter
 import suwayomi.tachidesk.manga.impl.util.getChapterCachePath
 import suwayomi.tachidesk.manga.impl.util.getChapterDownloadPath
+import suwayomi.tachidesk.manga.impl.util.storage.FileDeletionHelper
+import suwayomi.tachidesk.server.ApplicationDirs
 import java.io.File
 import java.io.FileInputStream
 import java.io.InputStream
+
+private val applicationDirs by DI.global.instance<ApplicationDirs>()
 
 /*
 * Provides downloaded files when pages were downloaded into folders
@@ -42,18 +49,14 @@ class FolderProvider(mangaId: Int, chapterId: Int) : ChaptersFilesProvider(manga
     }
 
     override fun delete(): Boolean {
-        val chapterDir = getChapterDownloadPath(mangaId, chapterId)
-        return File(chapterDir).deleteRecursively()
-    }
+        val chapterDirPath = getChapterDownloadPath(mangaId, chapterId)
+        val chapterDir = File(chapterDirPath)
+        if (!chapterDir.exists()) {
+            return true
+        }
 
-    private fun isExistingFile(
-        folder: File,
-        fileName: String,
-    ): Boolean {
-        val existingFile =
-            folder.listFiles { file ->
-                file.isFile && file.name.startsWith(fileName)
-            }?.firstOrNull()
-        return existingFile?.exists() == true
+        val chapterDirDeleted = chapterDir.deleteRecursively()
+        FileDeletionHelper.cleanupParentFoldersFor(chapterDir, applicationDirs.mangaDownloadsRoot)
+        return chapterDirDeleted
     }
 }
