Suwayomi/Suwayomi-Server

commit 729385588a3d8e06ec8be38865a12c47e88f6bcb

Prevent greater last page read than page count (#938)
In case multiple chapters are getting updated, the last page read might be higher than the available pages of a chapter

server/src/main/kotlin/suwayomi/tachidesk/graphql/mutations/ChapterMutation.kt
@@ -1,11 +1,12 @@
 package suwayomi.tachidesk.graphql.mutations
 
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.statements.BatchUpdateStatement
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.jetbrains.exposed.sql.update
 import suwayomi.tachidesk.graphql.types.ChapterMetaType
 import suwayomi.tachidesk.graphql.types.ChapterType
 import suwayomi.tachidesk.manga.impl.Chapter
@@ -55,19 +56,34 @@ class ChapterMutation {
         patch: UpdateChapterPatch,
     ) {
         transaction {
+            val chapterIdToPageCount =
+                if (patch.lastPageRead != null) {
+                    ChapterTable
+                        .slice(ChapterTable.id, ChapterTable.pageCount)
+                        .select { ChapterTable.id inList ids }
+                        .groupBy { it[ChapterTable.id].value }
+                        .mapValues { it.value.firstOrNull()?.let { it[ChapterTable.pageCount] } }
+                } else {
+                    emptyMap()
+                }
             if (patch.isRead != null || patch.isBookmarked != null || patch.lastPageRead != null) {
                 val now = Instant.now().epochSecond
-                ChapterTable.update({ ChapterTable.id inList ids }) { update ->
-                    patch.isRead?.also {
-                        update[isRead] = it
+
+                BatchUpdateStatement(ChapterTable).apply {
+                    ids.forEach { chapterId ->
+                        addBatch(EntityID(chapterId, ChapterTable))
+                        patch.isRead?.also {
+                            this[ChapterTable.isRead] = it
+                        }
+                        patch.isBookmarked?.also {
+                            this[ChapterTable.isBookmarked] = it
+                        }
+                        patch.lastPageRead?.also {
+                            this[ChapterTable.lastPageRead] = it.coerceAtMost(chapterIdToPageCount[it] ?: 0).coerceAtLeast(0)
+                            this[ChapterTable.lastReadAt] = now
+                        }
                     }
-                    patch.isBookmarked?.also {
-                        update[isBookmarked] = it
-                    }
-                    patch.lastPageRead?.also {
-                        update[lastPageRead] = it
-                        update[lastReadAt] = now
-                    }
+                    execute(this@transaction)
                 }
             }
         }

commit b793f39b0553aa29385d0801aefcabae3004aa11

Update lastPageRead on chapter update
Broken with 729385588a3d8e06ec8be38865a12c47e88f6bcb...

server/src/main/kotlin/suwayomi/tachidesk/graphql/mutations/ChapterMutation.kt
@@ -79,7 +79,7 @@ class ChapterMutation {
                             this[ChapterTable.isBookmarked] = it
                         }
                         patch.lastPageRead?.also {
-                            this[ChapterTable.lastPageRead] = it.coerceAtMost(chapterIdToPageCount[it] ?: 0).coerceAtLeast(0)
+                            this[ChapterTable.lastPageRead] = it.coerceAtMost(chapterIdToPageCount[chapterId] ?: 0).coerceAtLeast(0)
                             this[ChapterTable.lastReadAt] = now
                         }
                     }