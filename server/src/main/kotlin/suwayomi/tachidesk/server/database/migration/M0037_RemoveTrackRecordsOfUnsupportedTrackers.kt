d9c6a5f3ee2d9f06f85e8b222edaeb4930944240
schroda <50052685+schroda@users.noreply.github.com>
2024-05-06 14:47:24 +0200

    Delete track records of unsupporter trackers

---
+++ server/src/main/kotlin/suwayomi/tachidesk/server/database/migration/M0037_RemoveTrackRecordsOfUnsupportedTrackers.kt
@@ -0,0 +1,19 @@
+package suwayomi.tachidesk.server.database.migration
+
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import de.neonew.exposed.migrations.helpers.SQLMigration
+import suwayomi.tachidesk.manga.impl.track.tracker.TrackerManager
+
+@Suppress("ClassName", "unused")
+class M0037_RemoveTrackRecordsOfUnsupportedTrackers : SQLMigration() {
+    override val sql: String =
+        """
+        DELETE FROM TRACKRECORD WHERE SYNC_ID NOT IN (${TrackerManager.MYANIMELIST}, ${TrackerManager.ANILIST}, ${TrackerManager.MANGA_UPDATES})
+        """.trimIndent()
+}
