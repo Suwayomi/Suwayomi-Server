ee5474f53501b2700dac1c8dd2db366cd295197d
schroda <50052685+schroda@users.noreply.github.com>
2024-07-25 17:17:41 +0200

    Mark downloaded chapters without page count as not downloaded

---
+++ server/src/main/kotlin/suwayomi/tachidesk/server/database/migration/M0041_FixDownloadedChaptersWithoutPageCount.kt
@@ -0,0 +1,23 @@
+package suwayomi.tachidesk.server.database.migration
+
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import de.neonew.exposed.migrations.helpers.SQLMigration
+
+@Suppress("ClassName", "unused")
+class M0041_FixDownloadedChaptersWithoutPageCount : SQLMigration() {
+    override val sql: String =
+        """
+        UPDATE CHAPTER
+        SET IS_DOWNLOADED = FALSE
+        WHERE ID IN (
+            SELECT ID FROM CHAPTER
+            WHERE IS_DOWNLOADED = TRUE AND PAGE_COUNT <= 0
+        );
+        """.trimIndent()
+}
