e28e6fb1638befe51e8ed3120733f63e14f6fa70
Antoine Aflalo <197810+Belphemur@users.noreply.github.com>
2024-08-29 14:09:06 -0400

    feat(comicinfo): add date fields to comic info
    This will be parsed by Komga, Kavita etc ... and any other library management to also have the date of the chapter.

--- server/src/main/kotlin/suwayomi/tachidesk/manga/impl/util/GetComicInfo.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/manga/impl/util/GetComicInfo.kt
@@ -15,46 +15,56 @@ import suwayomi.tachidesk.manga.model.table.MangaTable
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 import java.nio.file.Path
+import java.time.Instant
+import java.time.ZoneId
 import kotlin.io.path.deleteIfExists
 import kotlin.io.path.div
 import kotlin.io.path.outputStream
 
-/**
- * Creates a ComicInfo instance based on the manga and chapter metadata.
- */
 fun getComicInfo(
     manga: ResultRow,
     chapter: ResultRow,
     chapterUrl: String,
     categories: List<String>?,
-) = ComicInfo(
-    title = ComicInfo.Title(chapter[ChapterTable.name]),
-    series = ComicInfo.Series(manga[MangaTable.title]),
-    number =
-        chapter[ChapterTable.chapter_number].takeIf { it >= 0 }?.let {
-            if ((it.rem(1) == 0.0f)) {
-                ComicInfo.Number(it.toInt().toString())
-            } else {
-                ComicInfo.Number(it.toString())
-            }
-        },
-    web = ComicInfo.Web(chapterUrl),
-    summary = manga[MangaTable.description]?.let { ComicInfo.Summary(it) },
-    writer = manga[MangaTable.author]?.let { ComicInfo.Writer(it) },
-    penciller = manga[MangaTable.artist]?.let { ComicInfo.Penciller(it) },
-    translator = chapter[ChapterTable.scanlator]?.let { ComicInfo.Translator(it) },
-    genre = manga[MangaTable.genre]?.let { ComicInfo.Genre(it) },
-    publishingStatus =
-        ComicInfo.PublishingStatusTachiyomi(
-            ComicInfoPublishingStatus.toComicInfoValue(manga[MangaTable.status].toLong()),
-        ),
-    categories = categories?.let { ComicInfo.CategoriesTachiyomi(it.joinToString()) },
-    inker = null,
-    colorist = null,
-    letterer = null,
-    coverArtist = null,
-    tags = null,
-)
+): ComicInfo {
+    val dateUpload = chapter[ChapterTable.date_upload]
+    val localDate =
+        dateUpload.let {
+            Instant.ofEpochMilli(it).atZone(ZoneId.systemDefault()).toLocalDate()
+        }
+
+    return ComicInfo(
+        title = ComicInfo.Title(chapter[ChapterTable.name]),
+        series = ComicInfo.Series(manga[MangaTable.title]),
+        number =
+            chapter[ChapterTable.chapter_number].takeIf { it >= 0 }?.let {
+                if ((it.rem(1) == 0.0f)) {
+                    ComicInfo.Number(it.toInt().toString())
+                } else {
+                    ComicInfo.Number(it.toString())
+                }
+            },
+        web = ComicInfo.Web(chapterUrl),
+        summary = manga[MangaTable.description]?.let { ComicInfo.Summary(it) },
+        writer = manga[MangaTable.author]?.let { ComicInfo.Writer(it) },
+        penciller = manga[MangaTable.artist]?.let { ComicInfo.Penciller(it) },
+        translator = chapter[ChapterTable.scanlator]?.let { ComicInfo.Translator(it) },
+        genre = manga[MangaTable.genre]?.let { ComicInfo.Genre(it) },
+        publishingStatus =
+            ComicInfo.PublishingStatusTachiyomi(
+                ComicInfoPublishingStatus.toComicInfoValue(manga[MangaTable.status].toLong()),
+            ),
+        categories = categories?.let { ComicInfo.CategoriesTachiyomi(it.joinToString()) },
+        inker = null,
+        colorist = null,
+        letterer = null,
+        coverArtist = null,
+        tags = null,
+        day = localDate?.dayOfMonth.let { it?.let { it1 -> ComicInfo.Day(it1) } },
+        month = localDate?.monthValue.let { it?.let { it1 -> ComicInfo.Month(it1) } },
+        year = localDate?.year.let { it?.let { it1 -> ComicInfo.Year(it1) } },
+    )
+}
 
 /**
  * Creates a ComicInfo.xml file inside the given directory.
