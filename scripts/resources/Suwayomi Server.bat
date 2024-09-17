ac21bd5fcf016f797e00f4ae0f340384a1c03aa5
BrutuZ <BrutuZ@users.noreply.github.com>
2024-09-16 21:04:10 -0300

    Create Suwayomi Server.bat
    Windows script to launch the server directly, with optional auto-updater

---
+++ scripts/resources/Suwayomi Server.bat
@@ -0,0 +1,22 @@
+@echo off
+set auto_update=1
+
+:: Move CWD to script folder
+pushd "%~0\.."
+
+:: Auto-Updater
+if %auto_update% NEQ 1 goto :launch
+for /f "tokens=2" %%i in ('curl -s https://api.github.com/repos/Suwayomi/Suwayomi-Server-preview/releases/latest^|findstr "https://.*jar"') do (
+	pushd bin
+	if NOT EXIST "%%~nxi" (
+    echo Updating to %%~ni
+		del /q "*.jar"
+		curl -sL %%i -o "%%~nxi"
+		mklink /h "Suwayomi-Server.jar" "%%~nxi"
+	)
+	popd
+)
+
+:launch
+start "" jre\bin\javaw %* -jar bin\Suwayomi-Server.jar
+popd
