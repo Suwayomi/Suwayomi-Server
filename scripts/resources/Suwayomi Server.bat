272e77dfdf84413fc8fe0d4270356c979d222390
BrutuZ <BrutuZ@users.noreply.github.com>
2024-09-16 21:56:37 -0300

    Update channel setting

--- scripts/resources/Suwayomi Server.bat
+++ scripts/resources/Suwayomi Server.bat
@@ -1,12 +1,18 @@
 @echo off
+SETLOCAL
+:: Auto Updater: 0: Disabled; 1 = Enabled
 set auto_update=1
+:: Update channel: 0 = Preview; 1 = Stable
+set preview=0
 
 :: Move CWD to script folder
 pushd "%~0\.."
 
 :: Auto-Updater
-if %auto_update% NEQ 1 goto :launch
-for /f "tokens=2" %%i in ('curl -s https://api.github.com/repos/Suwayomi/Suwayomi-Server-preview/releases/latest^|findstr "https://.*jar"') do (
+if (%auto_update% NEQ 1) goto :launch
+set channel=
+if (%preview% EQU 1) (set channel=-preview)
+for /f "tokens=2" %%i in ('curl -s https://api.github.com/repos/Suwayomi/Suwayomi-Server%channel%/releases/latest^|findstr "https://.*jar"') do (
 	pushd bin
 	if NOT EXIST "%%~nxi" (
     echo Updating to %%~ni
@@ -20,3 +26,4 @@ for /f "tokens=2" %%i in ('curl -s https://api.github.com/repos/Suwayomi/Suwayom
 :launch
 start "" jre\bin\javaw %* -jar Suwayomi-Launcher.jar --launch
 popd
+ENDLOCAL
