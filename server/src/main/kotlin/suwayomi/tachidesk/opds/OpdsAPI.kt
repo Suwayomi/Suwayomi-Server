713015e82d77e54ff9dfc03b350291d59e11ab0a
Zeedif <carlos_antonio-rl@hotmail.com>
2025-02-09 09:52:55 -0600

    Translate comments in OpdsAPI.kt to English

--- server/src/main/kotlin/suwayomi/tachidesk/opds/OpdsAPI.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/opds/OpdsAPI.kt
@@ -1,12 +1,5 @@
 package suwayomi.tachidesk.opds
 
-/*
- * Copyright (C) Contributors to the Suwayomi project
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.apibuilder.ApiBuilder.path
 import suwayomi.tachidesk.opds.controller.OpdsV1Controller
@@ -14,17 +7,17 @@ import suwayomi.tachidesk.opds.controller.OpdsV1Controller
 object OpdsAPI {
     fun defineEndpoints() {
         path("opds/v1.2") {
-            // Feed raíz (Navigation Feed)
+            // Root feed (Navigation Feed)
             get(OpdsV1Controller.rootFeed)
 
-            // Búsqueda
+            // Search
             get("search", OpdsV1Controller.searchFeed)
             get("search.xml", OpdsV1Controller.searchDescription)
 
-            // Feed completo para crawlers
-//            get("complete", OpdsV1Controller.completeFeed)
+            // Complete feed for crawlers
+            // get("complete", OpdsV1Controller.completeFeed)
 
-            // Agrupaciones principales
+            // Main groupings
             get("mangas", OpdsV1Controller.mangasFeed)
             get("sources", OpdsV1Controller.sourcesFeed)
             get("categories", OpdsV1Controller.categoriesFeed)
@@ -32,7 +25,7 @@ object OpdsAPI {
             get("status", OpdsV1Controller.statusFeed)
             get("languages", OpdsV1Controller.languagesFeed)
 
-            // Feeds por facetas (Acquisition Feeds)
+            // Faceted feeds (Acquisition Feeds)
             path("manga/{mangaId}") {
                 get(OpdsV1Controller.mangaFeed)
             }
