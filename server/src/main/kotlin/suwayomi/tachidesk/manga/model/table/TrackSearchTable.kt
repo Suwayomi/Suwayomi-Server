c6713e6e3157a5666d41cdc7caabb4d955f10946
schroda <50052685+schroda@users.noreply.github.com>
2025-05-30 15:51:49 +0200

    Automatically truncate required varchar columns

--- server/src/main/kotlin/suwayomi/tachidesk/manga/model/table/TrackSearchTable.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/manga/model/table/TrackSearchTable.kt
@@ -16,18 +16,19 @@ import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.statements.BatchUpdateStatement
 import org.jetbrains.exposed.sql.transactions.transaction
 import suwayomi.tachidesk.manga.impl.track.tracker.model.TrackSearch
+import suwayomi.tachidesk.manga.model.table.columns.truncatingVarchar
 
 object TrackSearchTable : IntIdTable() {
     val trackerId = integer("tracker_id")
     val remoteId = long("remote_id")
-    val title = varchar("title", 512)
+    val title = truncatingVarchar("title", 512)
     val totalChapters = integer("total_chapters")
-    val trackingUrl = varchar("tracking_url", 512)
-    val coverUrl = varchar("cover_url", 512)
-    val summary = varchar("summary", 4096)
-    val publishingStatus = varchar("publishing_status", 512)
-    val publishingType = varchar("publishing_type", 512)
-    val startDate = varchar("start_date", 128)
+    val trackingUrl = truncatingVarchar("tracking_url", 512)
+    val coverUrl = truncatingVarchar("cover_url", 512)
+    val summary = truncatingVarchar("summary", 4096)
+    val publishingStatus = truncatingVarchar("publishing_status", 512)
+    val publishingType = truncatingVarchar("publishing_type", 512)
+    val startDate = truncatingVarchar("start_date", 128)
 }
 
 fun List<TrackSearch>.insertAll(): List<ResultRow> {
@@ -63,14 +64,14 @@ fun List<TrackSearch>.insertAll(): List<ResultRow> {
                 toUpdate.forEach { (id, trackSearch) ->
                     id ?: return@forEach
                     addBatch(EntityID(id, TrackSearchTable))
-                    this[TrackSearchTable.title] = trackSearch.title.take(512)
+                    this[TrackSearchTable.title] = trackSearch.title
                     this[TrackSearchTable.totalChapters] = trackSearch.total_chapters
-                    this[TrackSearchTable.trackingUrl] = trackSearch.tracking_url.take(512)
-                    this[TrackSearchTable.coverUrl] = trackSearch.cover_url.take(512)
-                    this[TrackSearchTable.summary] = trackSearch.summary.take(4096)
-                    this[TrackSearchTable.publishingStatus] = trackSearch.publishing_status.take(512)
-                    this[TrackSearchTable.publishingType] = trackSearch.publishing_type.take(512)
-                    this[TrackSearchTable.startDate] = trackSearch.start_date.take(128)
+                    this[TrackSearchTable.trackingUrl] = trackSearch.tracking_url
+                    this[TrackSearchTable.coverUrl] = trackSearch.cover_url
+                    this[TrackSearchTable.summary] = trackSearch.summary
+                    this[TrackSearchTable.publishingStatus] = trackSearch.publishing_status
+                    this[TrackSearchTable.publishingType] = trackSearch.publishing_type
+                    this[TrackSearchTable.startDate] = trackSearch.start_date
                 }
                 execute(this@transaction)
             }
@@ -80,14 +81,14 @@ fun List<TrackSearch>.insertAll(): List<ResultRow> {
                 TrackSearchTable.batchInsert(toInsert) {
                     this[TrackSearchTable.trackerId] = it.sync_id
                     this[TrackSearchTable.remoteId] = it.media_id
-                    this[TrackSearchTable.title] = it.title.take(512)
+                    this[TrackSearchTable.title] = it.title
                     this[TrackSearchTable.totalChapters] = it.total_chapters
-                    this[TrackSearchTable.trackingUrl] = it.tracking_url.take(512)
-                    this[TrackSearchTable.coverUrl] = it.cover_url.take(512)
-                    this[TrackSearchTable.summary] = it.summary.take(4096)
-                    this[TrackSearchTable.publishingStatus] = it.publishing_status.take(512)
-                    this[TrackSearchTable.publishingType] = it.publishing_type.take(512)
-                    this[TrackSearchTable.startDate] = it.start_date.take(128)
+                    this[TrackSearchTable.trackingUrl] = it.tracking_url
+                    this[TrackSearchTable.coverUrl] = it.cover_url
+                    this[TrackSearchTable.summary] = it.summary
+                    this[TrackSearchTable.publishingStatus] = it.publishing_status
+                    this[TrackSearchTable.publishingType] = it.publishing_type
+                    this[TrackSearchTable.startDate] = it.start_date
                 }
             } else {
                 emptyList()
