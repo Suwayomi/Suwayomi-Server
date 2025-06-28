a89a2dfc18d584d5df870cbdde6b67756af652f9
Constantin Piber <cp.piber@gmail.com>
2025-06-28 18:25:05 +0200

    systemd: use startup script

--- scripts/resources/pkg/suwayomi-server.sh
+++ scripts/resources/pkg/suwayomi-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export LD_PRELOAD="/usr/share/java/suwayomi-server/bin/catch_abort.so"
-exec /usr/bin/java -jar /usr/share/java/suwayomi-server/bin/Suwayomi-Server.jar
+exec /usr/bin/java "$@" -jar /usr/share/java/suwayomi-server/bin/Suwayomi-Server.jar
