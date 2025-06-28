ae904753f784f61e25dc06c013497ee1c7b43701
Constantin Piber <59023762+cpiber@users.noreply.github.com>
2025-06-28 23:04:13 +0200

    systemd: use startup script, X server (#1482)
    * systemd: use startup script
    
    * script: Start X server using `xvfb-run` if DISPLAY is not set

--- scripts/resources/pkg/suwayomi-server.sh
+++ scripts/resources/pkg/suwayomi-server.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 export LD_PRELOAD="/usr/share/java/suwayomi-server/bin/catch_abort.so"
-exec /usr/bin/java -jar /usr/share/java/suwayomi-server/bin/Suwayomi-Server.jar
+
+if [ -z "$DISPLAY" ] && command -v Xvfb >/dev/null; then
+  echo "-- START: Spawning X server using xvfb-run --"
+  exec xvfb-run /usr/bin/java "$@" -jar /usr/share/java/suwayomi-server/bin/Suwayomi-Server.jar
+else
+  exec /usr/bin/java "$@" -jar /usr/share/java/suwayomi-server/bin/Suwayomi-Server.jar
+fi
