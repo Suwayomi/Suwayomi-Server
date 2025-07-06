62ef85df867e3b7084e6c52fc94045c5a772caf4
Constantin Piber <cp.piber@gmail.com>
2025-07-06 13:08:22 +0200

    [#1497] WebView: Localstorage

--- server/src/main/kotlin/suwayomi/tachidesk/server/ServerSetup.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/server/ServerSetup.kt
@@ -405,7 +405,10 @@ fun applicationSetup() {
                     }
                 }
                 download { github() }
-                settings { windowlessRenderingEnabled = true }
+                settings {
+                    windowlessRenderingEnabled = true
+                    cachePath = (Path(applicationDirs.dataRoot) / "cache/kcef").toString()
+                }
                 appHandler(
                     KCEF.AppHandler(
                         arrayOf("--disable-gpu", "--off-screen-rendering-enabled", "--disable-dev-shm-usage"),
