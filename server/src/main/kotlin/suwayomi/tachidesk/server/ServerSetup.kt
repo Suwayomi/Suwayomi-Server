ef07966a41bea550f7310728f907cb28d4e7189d
Syer10 <syer10@users.noreply.github.com>
2025-08-25 17:11:20 -0400

    Run the first databaseUp sync

--- server/src/main/kotlin/suwayomi/tachidesk/server/ServerSetup.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/server/ServerSetup.kt
@@ -372,6 +372,10 @@ fun applicationSetup() {
         "Localization service initialized. Supported languages: ${LocalizationHelper.getSupportedLocales()}"
     }
 
+    databaseUp()
+
+    LocalSource.register()
+
     serverConfig.subscribeTo(
         combine<Any, DatabaseSettings>(
             serverConfig.databaseType,
@@ -394,7 +398,7 @@ fun applicationSetup() {
 
             LocalSource.register()
         },
-        ignoreInitialValue = false,
+        ignoreInitialValue = true,
     )
 
     // create system tray
