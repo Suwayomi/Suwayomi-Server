f2b0973dbda27b14e005d7a8fb454257248a7670
schroda <50052685+schroda@users.noreply.github.com>
2025-10-04 23:29:56 +0200

    Extract source backup logic into BackupSourceHandler

---
+++ server/src/main/kotlin/suwayomi/tachidesk/manga/impl/backup/proto/handlers/BackupSourceHandler.kt
@@ -0,0 +1,57 @@
+package suwayomi.tachidesk.manga.impl.backup.proto.handlers
+
+/*
+ * Copyright (C) Contributors to the Suwayomi project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.selectAll
+import suwayomi.tachidesk.manga.impl.Source
+import suwayomi.tachidesk.manga.impl.Source.modifySourceMetas
+import suwayomi.tachidesk.manga.impl.backup.BackupFlags
+import suwayomi.tachidesk.manga.impl.backup.proto.models.BackupSource
+import suwayomi.tachidesk.manga.model.table.MangaTable
+import suwayomi.tachidesk.manga.model.table.SourceTable
+import suwayomi.tachidesk.server.database.dbTransaction
+
+object BackupSourceHandler {
+    fun backup(
+        mangas: List<ResultRow>,
+        flags: BackupFlags,
+    ): List<BackupSource> =
+        dbTransaction {
+            val inLibraryMangaSourceIds =
+                mangas
+                    .asSequence()
+                    .map { it[MangaTable.sourceReference] }
+                    .distinct()
+                    .toList()
+            val sources = SourceTable.selectAll().where { SourceTable.id inList inLibraryMangaSourceIds }
+            val sourceToMeta =
+                if (flags.includeClientData) {
+                    Source.getSourcesMetaMaps(sources.map { it[SourceTable.id].value })
+                } else {
+                    emptyMap()
+                }
+
+            inLibraryMangaSourceIds
+                .map { mangaSourceId ->
+                    val source = sources.firstOrNull { it[SourceTable.id].value == mangaSourceId }
+                    BackupSource(
+                        source?.get(SourceTable.name) ?: "",
+                        mangaSourceId,
+                    ).apply {
+                        if (flags.includeClientData) {
+                            this.meta = sourceToMeta[mangaSourceId] ?: emptyMap()
+                        }
+                    }
+                }.toList()
+        }
+
+    fun restore(backupSources: List<BackupSource>) {
+        modifySourceMetas(backupSources.associateBy { it.sourceId }.mapValues { it.value.meta })
+    }
+}
