5be4d2a1044b0eaac8dba8fdf060ce1c4b4381e9
Constantin Piber <59023762+cpiber@users.noreply.github.com>
2025-10-17 18:07:18 +0200

    Minor bug fixes for Webview, Permission request support (#1723)
    * fix: Match URLs with trailing /
    
    * Handle permission requests and attempt to enable Widevine
    
    * Tie CEF loglevel to server debug logs
    
    * Lint
    
    * Add missing file
    
    Forgot to add in previous commits
    
    * Provide WebResourceResponse
    
    * Fix NullException if headers are not set
    
    * fix: Don't allow interception for initial page load
    
    fixes #1713
    
    * Lint

--- server/src/main/kotlin/suwayomi/tachidesk/server/ServerSetup.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/server/ServerSetup.kt
@@ -15,6 +15,7 @@ import com.typesafe.config.ConfigRenderOptions
 import com.typesafe.config.ConfigValue
 import com.typesafe.config.parser.ConfigDocument
 import dev.datlag.kcef.KCEF
+import dev.datlag.kcef.KCEFBuilder.Settings.LogSeverity
 import eu.kanade.tachiyomi.App
 import eu.kanade.tachiyomi.createAppModule
 import eu.kanade.tachiyomi.network.NetworkHelper
@@ -522,10 +523,11 @@ fun applicationSetup() {
                 settings {
                     windowlessRenderingEnabled = true
                     cachePath = (Path(applicationDirs.dataRoot) / "cache/kcef").toString()
+                    logSeverity = if (serverConfig.debugLogsEnabled.value) LogSeverity.Verbose else LogSeverity.Default
                 }
                 appHandler(
                     KCEF.AppHandler(
-                        arrayOf("--disable-gpu", "--off-screen-rendering-enabled", "--disable-dev-shm-usage"),
+                        arrayOf("--disable-gpu", "--off-screen-rendering-enabled", "--disable-dev-shm-usage", "--enable-widevine-cdm"),
                     ),
                 )
 
