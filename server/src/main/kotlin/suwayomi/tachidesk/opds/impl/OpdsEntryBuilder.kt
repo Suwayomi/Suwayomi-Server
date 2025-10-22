25e675156b6929656b2d276901b64b553d3e224c
Constantin Piber <cp.piber@gmail.com>
2025-10-22 19:43:02 +0200

    OPDS: Include length when offering CBZ download

--- server/src/main/kotlin/suwayomi/tachidesk/opds/impl/OpdsEntryBuilder.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/opds/impl/OpdsEntryBuilder.kt
@@ -336,6 +336,14 @@ object OpdsEntryBuilder {
             }
 
         val entryTitle = "$titlePrefix ${chapter.name}"
+        val cbzFileSize =
+            if (chapter.downloaded) {
+                withContext(Dispatchers.IO) {
+                    runCatching { ChapterDownloadHelper.getArchiveStreamWithSize(manga.id, chapter.id).second }.getOrNull()
+                }
+            } else {
+                null
+            }
 
         val links = mutableListOf<OpdsLinkXml>()
         chapter.url?.let {
@@ -350,6 +358,7 @@ object OpdsEntryBuilder {
                     "/api/v1/chapter/${chapter.id}/download?markAsRead=${serverConfig.opdsMarkAsReadOnDownload.value}",
                     OpdsConstants.TYPE_CBZ,
                     MR.strings.opds_linktitle_download_cbz.localized(locale),
+                    length = cbzFileSize!!,
                 ),
             )
         }
@@ -410,15 +419,6 @@ object OpdsEntryBuilder {
                 ),
             )
         }
-
-        val cbzFileSize =
-            if (chapter.downloaded) {
-                withContext(Dispatchers.IO) {
-                    runCatching { ChapterDownloadHelper.getArchiveStreamWithSize(manga.id, chapter.id).second }.getOrNull()
-                }
-            } else {
-                null
-            }
 
         return OpdsEntryXml(
             id = "urn:suwayomi:chapter:${chapter.id}:metadata$idSuffix",
