5dbb4859cb6177d6069a05242b016062e4e1ee27
Syer10 <syer10@users.noreply.github.com>
2025-11-08 18:40:05 -0500

    Set Jvm Targets in BuildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     `kotlin-dsl`
 }
@@ -8,4 +10,13 @@ repositories {
 
 dependencies {
     implementation(libs.zip4j)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions.jvmTarget = JvmTarget.JVM_21
 }
