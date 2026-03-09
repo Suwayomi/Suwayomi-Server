8bc076e68d98fb96f7bcbd9b6e306f27a0316dbf
Vicente <vicente@per>
2026-03-09 12:49:15 +0100

    Remove webview

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -12,7 +12,7 @@ const val MainClass = "suwayomi.tachidesk.MainKt"
 // should be bumped with each stable release
 val getTachideskVersion = { "v2.1.${getCommitCount()}" }
 
-val webUIRevisionTag = "r3071"
+val webUIRevisionTag = "r3095"
 
 private val getCommitCount = {
     runCatching {
