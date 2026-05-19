88b61123c8cb6df52c20f55159e82942239e96b2
Constantin Piber <cp.piber@gmail.com>
2026-05-19 22:20:59 +0200

    Exclude masstest unless explicitly requested

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -189,7 +189,11 @@ tasks {
     }
 
     test {
-        useJUnitPlatform()
+        useJUnitPlatform {
+            if (!project.hasProperty("masstest")) {
+                exclude("**/masstest/*")
+            }
+        }
         testLogging {
             showStandardStreams = true
             events("passed", "skipped", "failed")
