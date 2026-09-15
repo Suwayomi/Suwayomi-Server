d37230ee22909414e5b4f9967eea31606bd890a4
schroda <50052685+schroda@users.noreply.github.com>
2026-09-15 18:04:28 +0200

    Fix/extension update null pointer exception (#2344)
    * Remove extension update info only after successful update
    
    In case the extension update failed, the extension update info was not available anymore causing a NullPointerException
    
    * Improve potential extension update NullPointerException error
    
    * Update changelog

--- server/src/main/kotlin/suwayomi/tachidesk/manga/impl/extension/Extension.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/manga/impl/extension/Extension.kt
@@ -761,11 +761,18 @@ object Extension {
     }
 
     suspend fun updateExtension(pkgName: String): String {
-        val targetExtension = ExtensionsList.updateMap.remove(pkgName)!!
+        val targetExtension =
+            checkNotNull(ExtensionsList.updateMap[pkgName]) {
+                "Extension update information for \"$pkgName\" missing. Refetch the extensions"
+            }
 
         logger.debug { "Updating $pkgName to ${targetExtension.versionName}" }
 
-        return installExtension(pkgName, true)
+        val result = installExtension(pkgName, true)
+
+        ExtensionsList.updateMap.remove(pkgName)
+
+        return result
     }
 
     suspend fun getExtensionIcon(pkgName: String): Pair<InputStream, String> {
