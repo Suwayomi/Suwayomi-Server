806d5bd4f7ee8e520a5e8d3e79c9e863d247e461
Alb11747 <albertli_042@hotmail.com>
2026-09-23 18:03:25 -0400

    test: remove redundant extension filename checks

--- server/src/test/kotlin/suwayomi/tachidesk/manga/impl/extension/ExtensionPackageTest.kt
+++ server/src/test/kotlin/suwayomi/tachidesk/manga/impl/extension/ExtensionPackageTest.kt
@@ -1,40 +1,11 @@
 package suwayomi.tachidesk.manga.impl.extension
 
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageInfo
-import android.os.Bundle
 import suwayomi.tachidesk.manga.impl.util.AndroidManifestParser
 import java.nio.file.Path
 import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotEquals
 import kotlin.test.assertTrue
 
 class ExtensionPackageTest {
-    @Test
-    fun higherVersionCodeWithUnchangedVersionNameUsesDifferentArchive() {
-        val installed = jarPackage("tachiyomi-en.aquamanga-v1.4.65.jar", 65)
-        val update = jarPackage("tachiyomi-en.aquamanga-v1.4.65.jar", 104065)
-
-        assertNotEquals(installed.getApkName(), update.getApkName())
-        assertNotEquals("tachiyomi-en.aquamanga-v1.4.65.apk", update.getApkName())
-    }
-
-    @Test
-    fun apkAndJarOfSameReleaseUseSameArchiveName() {
-        val packageInfo =
-            PackageInfo().apply {
-                packageName = "eu.kanade.tachiyomi.extension.en.aquamanga"
-                versionName = "1.4.65"
-                versionCode = 104065
-                applicationInfo = ApplicationInfo().apply { metaData = Bundle() }
-            }
-        val apk = Extension.ExtensionPackage.Apk(Path.of("tachiyomi-en.aquamanga-v1.4.65.apk"), packageInfo)
-        val jar = jarPackage("tachiyomi-en.aquamanga-v1.4.65.jar", 104065)
-
-        assertEquals(apk.getApkName(), jar.getApkName())
-    }
-
     @Test
     fun unversionedUploadIncludesVersionNameAndCode() {
         val extension = jarPackage("extension.jar", 104065)
